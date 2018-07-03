1abcf2c3db9ba2c0d529cb3f2cce7df7db22996c
uchan-nos <uchan0@gmail.com>
2018-07-03 17:20:18 +0900

    change indent 4 -> 2

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -2,11 +2,11 @@
 #include  <Library/UefiLib.h>
 
 EFI_STATUS EFIAPI UefiMain(
-    IN EFI_HANDLE ImageHandle,
-    IN EFI_SYSTEM_TABLE *SystemTable
-    )
+  EFI_HANDLE ImageHandle,
+  EFI_SYSTEM_TABLE *SystemTable
+  )
 {
-    Print(L"Hello, Mikan World!\n");
-    while (1);
-    return EFI_SUCCESS;
+  Print(L"Hello, Mikan World!\n");
+  while (1);
+  return EFI_SUCCESS;
 }
