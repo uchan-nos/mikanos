5829bef876fb9cd029c85c144163fed2f29bb6fc
uchan-nos <uchan0@gmail.com>
2018-07-06 12:04:33 +0900

    change code style

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -2,10 +2,8 @@
 #include  <Library/UefiLib.h>
 
 EFI_STATUS EFIAPI UefiMain(
-  EFI_HANDLE ImageHandle,
-  EFI_SYSTEM_TABLE *SystemTable
-  )
-{
+    EFI_HANDLE image_handle,
+    EFI_SYSTEM_TABLE *system_table) {
   Print(L"Hello, Mikan World!\n");
   while (1);
   return EFI_SUCCESS;
