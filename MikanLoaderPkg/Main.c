1b099b9097d0a43ded0c1233570cfc9bf9d45835
uchan-nos <uchan0@gmail.com>
2018-07-06 18:59:25 +0900

    add range tag for review

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -7,6 +7,7 @@
 #include  <Protocol/DiskIo2.h>
 #include  <Protocol/BlockIo.h>
 
+// #@@range_begin(struct_memory_map)
 struct MemoryMap {
   UINTN buffer_size;
   VOID* buffer;
@@ -15,7 +16,9 @@ struct MemoryMap {
   UINTN descriptor_size;
   UINT32 descriptor_version;
 };
+// #@@range_end(struct_memory_map)
 
+// #@@range_begin(get_memory_map)
 EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
   if (map->buffer == NULL) {
     return EFI_BUFFER_TOO_SMALL;
@@ -29,7 +32,9 @@ EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
       &map->descriptor_size,
       &map->descriptor_version);
 }
+// #@@range_end(get_memory_map)
 
+// #@@range_begin(get_memory_type)
 const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
   switch (type) {
     case EfiReservedMemoryType: return L"EfiReservedMemoryType";
@@ -51,7 +56,9 @@ const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
     default: return L"InvalidMemoryType";
   }
 }
+// #@@range_end(get_memory_type)
 
+// #@@range_begin(save_memory_map)
 EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
   CHAR8 buf[256];
   UINTN len;
@@ -81,6 +88,7 @@ EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
 
   return EFI_SUCCESS;
 }
+// #@@range_end(save_memory_map)
 
 EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root) {
   EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
@@ -112,6 +120,7 @@ EFI_STATUS EFIAPI UefiMain(
     EFI_SYSTEM_TABLE *system_table) {
   Print(L"Hello, Mikan World!\n");
 
+  // #@@range_begin(main)
   CHAR8 memmap_buf[4096 * 4];
   struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
   GetMemoryMap(&memmap);
@@ -126,6 +135,7 @@ EFI_STATUS EFIAPI UefiMain(
 
   SaveMemoryMap(&memmap, memmap_file);
   memmap_file->Close(memmap_file);
+  // #@@range_end(main)
 
   Print(L"All done\n");
 
