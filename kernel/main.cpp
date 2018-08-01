72ee3094627fa2f763ef657b931f1aa953d55f0f
uchan-nos <uchan0@gmail.com>
2018-08-01 18:05:07 +0900

    add a font for 'A'

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -9,6 +9,26 @@
 
 #include "frame_buffer_config.hpp"
 
+const uint8_t FontA[16] = {
+  0b00000000,
+  0b00011000,
+  0b00011000,
+  0b00011000,
+  0b00011000,
+  0b00100100,
+  0b00100100,
+  0b00100100,
+  0b00100100,
+  0b01111110,
+  0b01000010,
+  0b01000010,
+  0b01000010,
+  0b11100111,
+  0b00000000,
+  0b00000000,
+};
+
+// #@@range_begin(write_pixel)
 struct PixelColor {
   uint8_t r, g, b;
 };
