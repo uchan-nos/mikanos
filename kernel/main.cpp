aafe68004762a4fdce43e5a421409c1f8b9853cc
uchan-nos <uchan0@gmail.com>
2018-08-04 14:48:56 +0900

    add review tag and doxygen comment

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,21 +1,33 @@
+/**
+ * @file main.cpp
+ *
+ * カーネル本体のプログラムを書いたファイル．
+ */
+
 #include <cstdint>
 #include <cstddef>
 
 #include "frame_buffer_config.hpp"
 
+// #@@range_begin(write_pixel)
 struct PixelColor {
   uint8_t r, g, b;
 };
 
+/** WritePixelは1つの点を描画します．
+ * @retval 0   成功
+ * @retval 非0 失敗
+ */
 int WritePixel(const FrameBufferConfig& config,
-                int x, int y, const PixelColor& c) {
+               int x, int y, const PixelColor& c) {
+  const int pixel_position = config.pixels_per_scan_line * y + x;
   if (config.pixel_format == kPixelRGBResv8BitPerColor) {
-    uint8_t* p = &config.frame_buffer[4 * (config.pixels_per_scan_line * y + x)];
+    uint8_t* p = &config.frame_buffer[4 * pixel_position];
     p[0] = c.r;
     p[1] = c.g;
     p[2] = c.b;
   } else if (config.pixel_format == kPixelBGRResv8BitPerColor) {
-    uint8_t* p = &config.frame_buffer[4 * (config.pixels_per_scan_line * y + x)];
+    uint8_t* p = &config.frame_buffer[4 * pixel_position];
     p[0] = c.b;
     p[1] = c.g;
     p[2] = c.r;
@@ -24,19 +36,20 @@ int WritePixel(const FrameBufferConfig& config,
   }
   return 0;
 }
+// #@@range_end(write_pixel)
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+  // #@@range_begin(call_write_pixel)
   for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
     for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-      uint8_t c = (x + y) % 256;
-      WritePixel(frame_buffer_config, x, y, {c, c, c});
+      WritePixel(frame_buffer_config, x, y, {255, 255, 255});
     }
   }
-
-  for (int x = 0; x < 50; ++x) {
-    for (int y = 0; y < 30; ++y) {
-      WritePixel(frame_buffer_config, x, y, {255, 0, 0});
+  for (int x = 0; x < 200; ++x) {
+    for (int y = 0; y < 100; ++y) {
+      WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
     }
   }
+  // #@@range_end(call_write_pixel)
   while (1) __asm__("hlt");
 }
