ceb4651ba5a6da1f36e9e60a26f44cf0ae5203ca
uchan-nos <uchan0@gmail.com>
2018-09-17 23:05:55 +0900

    add all ascii fonts

--- kernel/font.cpp
+++ kernel/font.cpp
@@ -6,34 +6,32 @@
 
 #include "font.hpp"
 
-const uint8_t kFontA[16] = {
-  0b00000000, //
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b01111110, //  ******
-  0b01000010, //  *    *
-  0b01000010, //  *    *
-  0b01000010, //  *    *
-  0b11100111, // ***  ***
-  0b00000000, //
-  0b00000000, //
-};
+// #@@range_begin(hankaku_bin)
+extern const uint8_t _binary_hankaku_bin_start;
+extern const uint8_t _binary_hankaku_bin_end;
+extern const uint8_t _binary_hankaku_bin_size;
 
+const uint8_t* GetFont(char c) {
+  auto index = 16 * static_cast<unsigned int>(c);
+  if (index >= reinterpret_cast<uintptr_t>(&_binary_hankaku_bin_size)) {
+    return nullptr;
+  }
+  return &_binary_hankaku_bin_start + index;
+}
+// #@@range_end(hankaku_bin)
+
+// #@@range_begin(write_ascii)
 void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color) {
-  if (c != 'A') {
+  const uint8_t* font = GetFont(c);
+  if (font == nullptr) {
     return;
   }
   for (int dy = 0; dy < 16; ++dy) {
     for (int dx = 0; dx < 8; ++dx) {
-      if ((kFontA[dy] << dx) & 0x80u) {
+      if ((font[dy] << dx) & 0x80u) {
         writer.Write(x + dx, y + dy, color);
       }
     }
   }
 }
+// #@@range_end(write_ascii)
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -46,7 +46,11 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     }
   }
 
-  WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
-  WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
+  // #@@range_begin(write_fonts)
+  int i = 0;
+  for (char c = '!'; c <= '~'; ++c, ++i) {
+    WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
+  }
+  // #@@range_end(write_fonts)
   while (1) __asm__("hlt");
 }
