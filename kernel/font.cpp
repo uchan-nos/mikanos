1706269c74e901944a5293b6956b1fc2cdb7f4f3
uchan-nos <uchan0@gmail.com>
2018-09-17 14:04:38 +0900

    split main.cpp into sub files

---
+++ kernel/font.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file font.cpp
+ *
+ * フォント描画のプログラムを集めたファイル.
+ */
+
+#include "font.hpp"
+
+const uint8_t kFontA[16] = {
+  0b00000000, //
+  0b00011000, //    **
+  0b00011000, //    **
+  0b00011000, //    **
+  0b00011000, //    **
+  0b00100100, //   *  *
+  0b00100100, //   *  *
+  0b00100100, //   *  *
+  0b00100100, //   *  *
+  0b01111110, //  ******
+  0b01000010, //  *    *
+  0b01000010, //  *    *
+  0b01000010, //  *    *
+  0b11100111, // ***  ***
+  0b00000000, //
+  0b00000000, //
+};
+
+void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color) {
+  if (c != 'A') {
+    return;
+  }
+  for (int dy = 0; dy < 16; ++dy) {
+    for (int dx = 0; dx < 8; ++dx) {
+      if ((kFontA[dy] << dx) & 0x80u) {
+        writer.Write(x + dx, y + dy, color);
+      }
+    }
+  }
+}
---
+++ kernel/font.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <cstdint>
+#include "graphics.hpp"
+
+void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color);
