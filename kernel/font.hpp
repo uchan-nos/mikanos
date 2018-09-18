a8313b1bd97ce14c53a3d6844bfaf308b66fff98
uchan-nos <uchan0@gmail.com>
2018-09-18 11:50:16 +0900

    implement WriteString

--- kernel/font.hpp
+++ kernel/font.hpp
@@ -4,3 +4,4 @@
 #include "graphics.hpp"
 
 void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color);
+void WriteString(PixelWriter& writer, int x, int y, const char* s, const PixelColor& color);
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -51,6 +51,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   for (char c = '!'; c <= '~'; ++c, ++i) {
     WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
   }
+  WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
   // #@@range_end(write_fonts)
   while (1) __asm__("hlt");
 }
