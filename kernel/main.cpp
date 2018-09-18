0eb1df9718545e06a1329c199eb29a808778cf80
uchan-nos <uchan0@gmail.com>
2018-09-18 23:11:05 +0900

    add Console class

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -12,6 +12,7 @@
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "font.hpp"
+#include "console.hpp"
 // #@@range_end(includes)
 
 void* operator new(size_t size, void* buf) {
@@ -41,24 +42,13 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
-  for (int x = 0; x < 200; ++x) {
-    for (int y = 0; y < 100; ++y) {
-      pixel_writer->Write(x, y, {0, 255, 0});
-    }
-  }
 
-  // #@@range_begin(write_fonts)
-  int i = 0;
-  for (char c = '!'; c <= '~'; ++c, ++i) {
-    WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
-  }
-  WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
-  // #@@range_end(write_fonts)
+  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-  // #@@range_begin(sprintf)
   char buf[128];
-  sprintf(buf, "1 + 2 = %d", 1 + 2);
-  WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
-  // #@@range_end(sprintf)
+  for (int i = 0; i < 27; ++i) {
+    sprintf(buf, "line %d\n", i);
+    console.PutString(buf);
+  }
   while (1) __asm__("hlt");
 }
