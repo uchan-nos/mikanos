ce101521d7d8739b734bb49a0422b7775a9c924c
uchan-nos <uchan0@gmail.com>
2018-09-19 23:47:36 +0900

    implement printk

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -25,6 +25,26 @@ void operator delete(void* obj) noexcept {
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
+// #@@range_begin(console_buf)
+char console_buf[sizeof(Console)];
+Console* console;
+// #@@range_end(console_buf)
+
+// #@@range_begin(printk)
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
+// #@@range_end(printk)
+
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
     case kPixelRGBResv8BitPerColor:
@@ -43,12 +63,14 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     }
   }
 
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  // #@@range_begin(new_console)
+  console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  // #@@range_end(new_console)
 
-  char buf[128];
+  // #@@range_begin(use_printk)
   for (int i = 0; i < 27; ++i) {
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+    printk("printk: %d\n", i);
   }
+  // #@@range_end(use_printk)
   while (1) __asm__("hlt");
 }
