e7991214cf8dd0a58aa62f05c22983cb50a13fc9
uchan-nos <uchan0@gmail.com>
2018-09-21 17:38:17 +0900

    add PCI device scanner

---
+++ kernel/asmfunc.asm
@@ -0,0 +1,20 @@
+; asmfunc.asm
+;
+; System V AMD64 Calling Convention
+; Registers: RDI, RSI, RDX, RCX, R8, R9
+
+bits 64
+section .text
+
+global IoOut32  ; void IoOut32(uint16_t addr, uint32_t data);
+IoOut32:
+    mov dx, di    ; dx = addr
+    mov eax, esi  ; eax = data
+    out dx, eax
+    ret
+
+global IoIn32  ; uint32_t IoIn32(uint16_t addr);
+IoIn32:
+    mov dx, di    ; dx = addr
+    in eax, dx
+    ret
---
+++ kernel/pci.hpp
@@ -0,0 +1,88 @@
+/**
+ * @file pci.hpp
+ *
+ * PCI バス制御のプログラムを集めたファイル．
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <array>
+
+#include "error.hpp"
+
+namespace pci {
+  /** @brief CONFIG_ADDRESS レジスタの IO ポートアドレス */
+  const uint16_t kConfigAddress = 0x0cf8;
+  /** @brief CONFIG_DATA レジスタの IO ポートアドレス */
+  const uint16_t kConfigData = 0x0cfc;
+
+  /** @brief CONFIG_ADDRESS 用の 32 ビット整数を生成する */
+  constexpr uint32_t MakeAddress(uint8_t bus, uint8_t device,
+                                 uint8_t function, uint8_t reg_addr) {
+    auto shl = [](uint32_t x, unsigned int bits) {
+        return x << bits;
+    };
+
+    return shl(1, 31)  // enable bit
+        | shl(bus, 16)
+        | shl(device, 11)
+        | shl(function, 8)
+        | (reg_addr & 0xfcu);
+  }
+
+  /** @brief CONFIG_ADDRESS に指定された整数を書き込む */
+  void WriteAddress(uint32_t address);
+  /** @brief CONFIG_DATA に指定された整数を書き込む */
+  void WriteData(uint32_t value);
+  /** @brief CONFIG_DATA から 32 ビット整数を読み込む */
+  uint32_t ReadData();
+
+  /** @brief ベンダ ID レジスタを読み取る（全ヘッダタイプ共通） */
+  uint16_t ReadVendorId(uint8_t bus, uint8_t device, uint8_t function);
+  /** @brief デバイス ID レジスタを読み取る（全ヘッダタイプ共通） */
+  uint16_t ReadDeviceId(uint8_t bus, uint8_t device, uint8_t function);
+  /** @brief ヘッダタイプレジスタを読み取る（全ヘッダタイプ共通） */
+  uint8_t ReadHeaderType(uint8_t bus, uint8_t device, uint8_t function);
+  /** @brief クラスコードレジスタを読み取る（全ヘッダタイプ共通）
+   *
+   * 返される 32 ビット整数の構造は次の通り．
+   *   - 31:24 : ベースクラス
+   *   - 23:16 : サブクラス
+   *   - 15:8  : インターフェース
+   *   - 7:0   : リビジョン
+   */
+  uint32_t ReadClassCode(uint8_t bus, uint8_t device, uint8_t function);
+
+  /** @brief バス番号レジスタを読み取る（ヘッダタイプ 1 用）
+   *
+   * 返される 32 ビット整数の構造は次の通り．
+   *   - 23:16 : サブオーディネイトバス番号
+   *   - 15:8  : セカンダリバス番号
+   *   - 7:0   : リビジョン番号
+   */
+  uint32_t ReadBusNumbers(uint8_t bus, uint8_t device, uint8_t function);
+
+  /** @brief 単一ファンクションの場合に真を返す． */
+  bool IsSingleFunctionDevice(uint8_t header_type);
+
+  /** @brief PCI デバイスを操作するための基礎データを格納する
+   *
+   * バス番号，デバイス番号，ファンクション番号はデバイスを特定するのに必須．
+   * その他の情報は単に利便性のために加えてある．
+   * */
+  struct Device {
+    uint8_t bus, device, function, header_type;
+  };
+
+  /** @brief ScanAllBus() により発見された PCI デバイスの一覧 */
+  extern std::array<Device, 32> devices;
+  /** @brief devices の有効な要素の数 */
+  extern int num_device;
+  /** @brief PCI デバイスをすべて探索し devices に格納する
+   *
+   * バス 0 から再帰的に PCI デバイスを探索し，devices の先頭から詰めて書き込む．
+   * 発見したデバイスの数を num_devices に設定する．
+   */
+  Error ScanAllBus();
+}
