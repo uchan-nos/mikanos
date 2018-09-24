2d7e8f74207df1836f60f9124a3ec27062495080
uchan-nos <uchan0@gmail.com>
2018-09-24 11:35:08 +0900

    add review tags

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -127,6 +127,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
         vendor_id, class_code, dev.header_type);
   }
 
+  // #@@range_begin(find_xhc)
   pci::Device* xhc_dev = nullptr;
   for (int i = 0; i < pci::num_device; ++i) {
     if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x30u)) {
@@ -140,6 +141,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     printk("xHC has been found: %d.%d.%d\n",
         xhc_dev->bus, xhc_dev->device, xhc_dev->function);
   }
+  // #@@range_end(find_xhc)
 
   while (1) __asm__("hlt");
 }
