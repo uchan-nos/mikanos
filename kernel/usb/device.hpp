19906c4ff5812d10e8a53949376d43745fa97579
uchan-nos <uchan0@gmail.com>
2018-10-09 08:01:09 +0900

    add device.cpp

--- kernel/usb/device.hpp
+++ kernel/usb/device.hpp
@@ -11,5 +11,12 @@
 namespace usb {
   class Device {
    public:
+    virtual Error ControlIn(int ep_num, uint64_t setup_data,
+                            void* buf, int len) = 0;
+    virtual Error ControlOut(int ep_num, uint64_t setup_data,
+                             const void* buf, int len) = 0;
   };
+
+  Error GetDescriptor(Device& dev, bool debug = false);
+  Error ConfigureEndpoints(Device& dev, bool debug = false);
 }
