1a5af4820ff2b6570fba6506059cb01978b47bc0
vagrant <vagrant@vagrant.vm>
2018-10-30 07:52:38 +0000

    receive key codes via default control pipe

--- kernel/usb/classdriver/base.hpp
+++ kernel/usb/classdriver/base.hpp
@@ -8,12 +8,28 @@
 
 #include "error.hpp"
 #include "usb/endpoint.hpp"
+#include "usb/setupdata.hpp"
 
 namespace usb {
+  class Device;
+
   class ClassDriver {
    public:
+    ClassDriver(Device* dev);
     virtual ~ClassDriver();
+
     virtual Error Initialize() = 0;
     virtual Error SetEndpoint(const EndpointConfig& config) = 0;
+    virtual Error OnEndpointsConfigured() = 0;
+    virtual Error OnControlOutCompleted(SetupData setup_data,
+                                        const void* buf, int len) = 0;
+    virtual Error OnControlInCompleted(SetupData setup_data,
+                                       const void* buf, int len) = 0;
+
+    /** このクラスドライバを保持する USB デバイスを返す． */
+    Device* ParentDevice() const { return dev_; }
+
+   private:
+    Device* dev_;
   };
 }
