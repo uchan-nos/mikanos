ecdb944b11119c8aff7798309bb854c482b69cc3
uchan-nos <uchan0@gmail.com>
2018-11-02 08:14:50 +0900

    continuous input from keyboard. buggy

--- kernel/usb/classdriver/keyboard.hpp
+++ kernel/usb/classdriver/keyboard.hpp
@@ -19,9 +19,13 @@ namespace usb {
                                 const void* buf, int len) override;
     Error OnControlInCompleted(SetupData setup_data,
                                const void* buf, int len) override;
+    Error OnInterruptOutCompleted(const void* buf, int len) override;
+    Error OnInterruptInCompleted(const void* buf, int len) override;
 
     void* operator new(size_t size);
     void operator delete(void* ptr) noexcept;
+
+    void SubscribeKeyPush(std::function<void (uint8_t keycode)> observer);
 
    private:
     int ep_interrupt_in_;
@@ -29,5 +33,10 @@ namespace usb {
 
     std::array<uint8_t, 8> buf_;
     std::array<uint8_t, 8> previous_buf_;
+
+    std::array<std::function<void (uint8_t keycode)>, 4> observers_;
+    int num_observers_ = 0;
+
+    void NotifyKeyPush(uint8_t keycode);
   };
 }
