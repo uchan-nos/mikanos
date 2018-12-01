55ceebf1c9e7b7f32b0218eb7186450ccc7f2374
uchan-nos <uchan0@gmail.com>
2018-12-01 16:59:11 +0900

    move mouse cursor according to usb input

---
+++ kernel/mouse.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file mouse.hpp
+ *
+ * マウス制御プログラム．
+ */
+
+#pragma once
+
+#include "graphics.hpp"
+
+class MouseCursor {
+ public:
+  MouseCursor(PixelWriter* writer, PixelColor erase_color,
+              Vector2D<int> initial_position);
+  void MoveRelative(Vector2D<int> displacement);
+
+ private:
+  PixelWriter* pixel_writer_ = nullptr;
+  PixelColor erase_color_;
+  Vector2D<int> position_;
+};
