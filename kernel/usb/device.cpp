ca1d889c7085c51b37d92568662ddbdaf01f6dd4
uchan-nos <uchan0@gmail.com>
2019-03-17 11:46:52 +0900

    add default observer variable for keyboard and mouse

--- kernel/usb/device.cpp
+++ kernel/usb/device.cpp
@@ -9,39 +9,6 @@
 #include "logger.hpp"
 
 int printk(const char* format, ...);
-usb::HIDMouseDriver* NewHIDMouseDriver(usb::Device* usb_device, int interface_index);
-
-const char keycode_map[256] = {
-  0,    0,    0,    0,  'a',  'b',  'c',  'd', // 0
-  'e',  'f',  'g',  'h',  'i',  'j',  'k',  'l', // 8
-  'm',  'n',  'o',  'p',  'q',  'r',  's',  't', // 16
-  'u',  'v',  'w',  'x',  'y',  'z',  '1',  '2', // 24
-  '3',  '4',  '5',  '6',  '7',  '8',  '9',  '0', // 32
-  '\n', '\b', 0x08, '\t',  ' ',  '-',  '=',  '[', // 40
-  ']', '\\',  '#',  ';', '\'',  '`',  ',',  '.', // 48
-  '/',    0,    0,    0,    0,    0,    0,    0, // 56
-  0,    0,    0,    0,    0,    0,    0,    0, // 64
-  0,    0,    0,    0,    0,    0,    0,    0, // 72
-  0,    0,    0,    0,  '/',  '*',  '-',  '+', // 80
-  '\n',  '1',  '2',  '3',  '4',  '5',  '6',  '7', // 88
-  '8',  '9',  '0',  '.', '\\',    0,    0,  '=', // 96
-};
-
-const char keycode_map_shifted[256] = {
-  0,    0,    0,    0,  'A',  'B',  'C',  'D', // 0
-  'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L', // 8
-  'M',  'N',  'O',  'P',  'Q',  'R',  'S',  'T', // 16
-  'U',  'V',  'W',  'X',  'Y',  'Z',  '!',  '@', // 24
-  '#',  '$',  '%',  '^',  '&',  '*',  '(',  ')', // 32
-  '\n', '\b', 0x08, '\t',  ' ',  '_',  '+',  '{', // 40
-  '}',  '|',  '~',  ':',  '"',  '~',  '<',  '>', // 48
-  '?',    0,    0,    0,    0,    0,    0,    0, // 56
-  0,    0,    0,    0,    0,    0,    0,    0, // 64
-  0,    0,    0,    0,    0,    0,    0,    0, // 72
-  0,    0,    0,    0,  '/',  '*',  '-',  '+', // 80
-  '\n',  '1',  '2',  '3',  '4',  '5',  '6',  '7', // 88
-  '8',  '9',  '0',  '.', '\\',    0,    0,  '=', // 96
-};
 
 namespace usb {
   Device::~Device() {
@@ -183,15 +150,12 @@ namespace usb {
           if_desc->interface_sub_class == 1) {  // HID boot interface
         if (if_desc->interface_protocol == 1) {  // keyboard
           auto keyboard_driver = new HIDKeyboardDriver{this, if_index};
-          keyboard_driver->SubscribeKeyPush([](uint8_t keycode) {
-            const char ascii = keycode_map[keycode];
-            if (ascii != '\0') {
-              printk("%c", ascii);
-            }
-          });
+          keyboard_driver->SubscribeKeyPush(usb::HIDKeyboardDriver::default_observer);
           class_driver = keyboard_driver;
         } else if (if_desc->interface_protocol == 2) {  // mouse
-          class_driver = NewHIDMouseDriver(this, if_index);
+          auto mouse_driver = new usb::HIDMouseDriver{this, if_index};
+          mouse_driver->SubscribeMouseMove(usb::HIDMouseDriver::default_observer);
+          class_driver = mouse_driver;
         }
       }
       if (class_driver == nullptr) {
