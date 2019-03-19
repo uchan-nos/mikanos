8d1406b44e0890d454cc8cfa4a25429c0ab5a3d2
uchan-nos <uchan0@gmail.com>
2019-03-19 10:05:45 +0900

    remove keyboard input functionality (temporary)

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -20,7 +20,6 @@
 #include "logger.hpp"
 #include "usb/memory.hpp"
 #include "usb/device.hpp"
-#include "usb/classdriver/keyboard.hpp"
 #include "usb/classdriver/mouse.hpp"
 #include "usb/xhci/xhci.hpp"
 #include "usb/xhci/trb.hpp"
@@ -48,45 +47,6 @@ int printk(const char* format, ...) {
 
   console->PutString(s);
   return result;
-}
-
-const char keycode_map[256] = {
-  0,    0,    0,    0,  'a',  'b',  'c',  'd', // 0
-  'e',  'f',  'g',  'h',  'i',  'j',  'k',  'l', // 8
-  'm',  'n',  'o',  'p',  'q',  'r',  's',  't', // 16
-  'u',  'v',  'w',  'x',  'y',  'z',  '1',  '2', // 24
-  '3',  '4',  '5',  '6',  '7',  '8',  '9',  '0', // 32
-  '\n', '\b', 0x08, '\t',  ' ',  '-',  '=',  '[', // 40
-  ']', '\\',  '#',  ';', '\'',  '`',  ',',  '.', // 48
-  '/',    0,    0,    0,    0,    0,    0,    0, // 56
-  0,    0,    0,    0,    0,    0,    0,    0, // 64
-  0,    0,    0,    0,    0,    0,    0,    0, // 72
-  0,    0,    0,    0,  '/',  '*',  '-',  '+', // 80
-  '\n',  '1',  '2',  '3',  '4',  '5',  '6',  '7', // 88
-  '8',  '9',  '0',  '.', '\\',    0,    0,  '=', // 96
-};
-
-const char keycode_map_shifted[256] = {
-  0,    0,    0,    0,  'A',  'B',  'C',  'D', // 0
-  'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L', // 8
-  'M',  'N',  'O',  'P',  'Q',  'R',  'S',  'T', // 16
-  'U',  'V',  'W',  'X',  'Y',  'Z',  '!',  '@', // 24
-  '#',  '$',  '%',  '^',  '&',  '*',  '(',  ')', // 32
-  '\n', '\b', 0x08, '\t',  ' ',  '_',  '+',  '{', // 40
-  '}',  '|',  '~',  ':',  '"',  '~',  '<',  '>', // 48
-  '?',    0,    0,    0,    0,    0,    0,    0, // 56
-  0,    0,    0,    0,    0,    0,    0,    0, // 64
-  0,    0,    0,    0,    0,    0,    0,    0, // 72
-  0,    0,    0,    0,  '/',  '*',  '-',  '+', // 80
-  '\n',  '1',  '2',  '3',  '4',  '5',  '6',  '7', // 88
-  '8',  '9',  '0',  '.', '\\',    0,    0,  '=', // 96
-};
-
-void KeyboardObserver(uint8_t keycode) {
-  const char ascii = keycode_map[keycode];
-  if (ascii != '\0') {
-    printk("%c", ascii);
-  }
 }
 
 char mouse_cursor_buf[sizeof(MouseCursor)];
@@ -180,7 +140,6 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   Log(kInfo, "xHC starting\n");
   xhc.Run();
 
-  usb::HIDKeyboardDriver::default_observer = KeyboardObserver;
   usb::HIDMouseDriver::default_observer = MouseObserver;
 
   for (int i = 1; i <= xhc.MaxPorts(); ++i) {
