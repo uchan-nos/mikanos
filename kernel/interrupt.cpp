e0250b378256b4ca80bf9ef37b1899d61db6dffe
uchan-nos <uchan0@gmail.com>
2019-03-29 14:11:17 +0900

    add IDT definition

---
+++ kernel/interrupt.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file interrupt.cpp
+ *
+ * 割り込み用のプログラムを集めたファイル．
+ */
+
+#include "interrupt.hpp"
+
+void SetIDTEntry(InterruptDescriptor& desc,
+                 InterruptDescriptorAttribute attr,
+                 uint64_t offset,
+                 uint16_t segment_selector) {
+  desc.attr = attr;
+  desc.offset_low = offset & 0xffffu;
+  desc.offset_middle = (offset >> 16) & 0xffffu;
+  desc.offset_high = offset >> 32;
+  desc.segment_selector = segment_selector;
+}
