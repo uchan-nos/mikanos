eb870f071d2d56f042b404ed4437faae7208a349
uchan-nos <uchan0@gmail.com>
2019-04-02 10:47:08 +0900

    interrupt success

--- kernel/interrupt.cpp
+++ kernel/interrupt.cpp
@@ -16,3 +16,14 @@ void SetIDTEntry(InterruptDescriptor& desc,
   desc.offset_high = offset >> 32;
   desc.segment_selector = segment_selector;
 }
+
+void LoadIDT() {
+  uint8_t idtr_buf[sizeof(uint16_t) + sizeof(uintptr_t)];
+  const uint16_t length = sizeof(idt) - 1;
+  const uintptr_t offset = reinterpret_cast<uintptr_t>(&idt[0]);
+
+  memcpy(&idtr_buf[0], &length, sizeof(uint16_t));
+  memcpy(&idtr_buf[sizeof(uint16_t)], &offset, sizeof(uintptr_t));
+
+  asm("lidt %0" : : "m"(idtr_buf));
+}
