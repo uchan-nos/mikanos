e2773e4b3b21852d13a8b3571689ee5d410463f1
uchan-nos <uchan0@gmail.com>
2019-04-12 08:48:29 +0900

    use asm function instead of inline asm for LIDT

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -24,3 +24,15 @@ GetCS:
     xor eax, eax  ; also clears upper 32 bits of rax
     mov ax, cs
     ret
+
+global LoadIDT  ; void LoadIDT(uint16_t limit, uint64_t offset);
+LoadIDT:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 10
+    mov [rsp], di  ; limit
+    mov [rsp + 2], rsi  ; offset
+    lidt [rsp]
+    mov rsp, rbp
+    pop rbp
+    ret
