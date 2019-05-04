6247593331c5fb44f69ee81755affae7bc4d9aa6
uchan-nos <uchan0@gmail.com>
2019-05-04 17:19:30 +0900

    Revert "add review tags"
    This reverts commit fd40783263ab0861e1557bd0cc2ebdd7a827c7fb.

--- kernel/interrupt.hpp
+++ kernel/interrupt.hpp
@@ -9,7 +9,6 @@
 #include <array>
 #include <cstdint>
 
-// #@@range_begin(desc_types)
 enum class DescriptorType {
   kUpper8Bytes   = 0,
   kLDT           = 2,
@@ -19,9 +18,7 @@ enum class DescriptorType {
   kInterruptGate = 14,
   kTrapGate      = 15,
 };
-// #@@range_end(desc_types)
 
-// #@@range_begin(descriptor_attr_struct)
 union InterruptDescriptorAttribute {
   uint16_t data;
   struct {
@@ -33,9 +30,7 @@ union InterruptDescriptorAttribute {
     uint16_t present : 1;
   } __attribute__((packed)) bits;
 } __attribute__((packed));
-// #@@range_end(descriptor_attr_struct)
 
-// #@@range_begin(descriptor_struct)
 struct InterruptDescriptor {
   uint16_t offset_low;
   uint16_t segment_selector;
@@ -44,11 +39,9 @@ struct InterruptDescriptor {
   uint32_t offset_high;
   uint32_t reserved;
 } __attribute__((packed));
-// #@@range_end(descriptor_struct)
 
 extern std::array<InterruptDescriptor, 256> idt;
 
-// #@@range_begin(make_idt_attr)
 constexpr InterruptDescriptorAttribute MakeIDTAttr(
     DescriptorType type,
     uint8_t descriptor_privilege_level,
@@ -61,23 +54,19 @@ constexpr InterruptDescriptorAttribute MakeIDTAttr(
   attr.bits.present = present;
   return attr;
 }
-// #@@range_end(make_idt_attr)
 
 void SetIDTEntry(InterruptDescriptor& desc,
                  InterruptDescriptorAttribute attr,
                  uint64_t offset,
                  uint16_t segment_selector);
 
-// #@@range_begin(vector_numbers)
 class InterruptVector {
  public:
   enum Number {
     kXHCI = 0x40,
   };
 };
-// #@@range_end(vector_numbers)
 
-// #@@range_begin(frame_struct)
 struct InterruptFrame {
   uint64_t rip;
   uint64_t cs;
@@ -85,6 +74,5 @@ struct InterruptFrame {
   uint64_t rsp;
   uint64_t ss;
 };
-// #@@range_end(frame_struct)
 
 void NotifyEndOfInterrupt();
