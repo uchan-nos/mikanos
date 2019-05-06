3167ecd2b00ac45ed43f8163bb5e5fa9f90ba73c
uchan-nos <uchan0@gmail.com>
2019-05-06 17:44:17 +0900

    move queue member implememtations out of the class

--- kernel/queue.hpp
+++ kernel/queue.hpp
@@ -9,50 +9,13 @@ template <typename T>
 class ArrayQueue {
  public:
   template <size_t N>
-  ArrayQueue(std::array<T, N>& buf) : ArrayQueue(buf.data(), N) {}
-
-  ArrayQueue(T* buf, size_t size)
-    : data_{buf}, read_pos_{0}, write_pos_{0}, count_{0}, capacity_{size}
-  {}
-
-  Error Push(const T& value) {
-    if (count_ == capacity_) {
-      return MAKE_ERROR(Error::kFull);
-    }
-
-    data_[write_pos_] = value;
-    ++count_;
-    ++write_pos_;
-    if (write_pos_ == capacity_) {
-      write_pos_ = 0;
-    }
-    return MAKE_ERROR(Error::kSuccess);
-  }
-
-  Error Pop() {
-    if (count_ == 0) {
-      return MAKE_ERROR(Error::kEmpty);
-    }
-
-    --count_;
-    ++read_pos_;
-    if (read_pos_ == capacity_) {
-      read_pos_ = 0;
-    }
-    return MAKE_ERROR(Error::kSuccess);
-  }
-
-  size_t Count() const {
-    return count_;
-  }
-
-  size_t Capacity() const {
-    return capacity_;
-  }
-
-  const T& Front() const {
-    return data_[read_pos_];
-  }
+  ArrayQueue(std::array<T, N>& buf);
+  ArrayQueue(T* buf, size_t size);
+  Error Push(const T& value);
+  Error Pop();
+  size_t Count() const;
+  size_t Capacity() const;
+  const T& Front() const;
 
  private:
   T* data_;
@@ -64,3 +27,56 @@ class ArrayQueue {
    */
   const size_t capacity_;
 };
+
+template <typename T>
+template <size_t N>
+ArrayQueue<T>::ArrayQueue(std::array<T, N>& buf) : ArrayQueue(buf.data(), N) {}
+
+template <typename T>
+ArrayQueue<T>::ArrayQueue(T* buf, size_t size)
+  : data_{buf}, read_pos_{0}, write_pos_{0}, count_{0}, capacity_{size}
+{}
+
+template <typename T>
+Error ArrayQueue<T>::Push(const T& value) {
+  if (count_ == capacity_) {
+    return MAKE_ERROR(Error::kFull);
+  }
+
+  data_[write_pos_] = value;
+  ++count_;
+  ++write_pos_;
+  if (write_pos_ == capacity_) {
+    write_pos_ = 0;
+  }
+  return MAKE_ERROR(Error::kSuccess);
+}
+
+template <typename T>
+Error ArrayQueue<T>::Pop() {
+  if (count_ == 0) {
+    return MAKE_ERROR(Error::kEmpty);
+  }
+
+  --count_;
+  ++read_pos_;
+  if (read_pos_ == capacity_) {
+    read_pos_ = 0;
+  }
+  return MAKE_ERROR(Error::kSuccess);
+}
+
+template <typename T>
+size_t ArrayQueue<T>::Count() const {
+  return count_;
+}
+
+template <typename T>
+size_t ArrayQueue<T>::Capacity() const {
+  return capacity_;
+}
+
+template <typename T>
+const T& ArrayQueue<T>::Front() const {
+  return data_[read_pos_];
+}
