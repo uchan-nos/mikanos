29eccce8dcab6087d3df645ab36aaaf8070ff44d
uchan-nos <uchan0@gmail.com>
2019-05-06 16:09:35 +0900

    add review tags

--- kernel/queue.hpp
+++ kernel/queue.hpp
@@ -5,6 +5,7 @@
 
 #include "error.hpp"
 
+// #@@range_begin(class)
 template <typename T>
 class ArrayQueue {
  public:
@@ -27,7 +28,9 @@ class ArrayQueue {
    */
   const size_t capacity_;
 };
+// #@@range_end(class)
 
+// #@@range_begin(constructor)
 template <typename T>
 template <size_t N>
 ArrayQueue<T>::ArrayQueue(std::array<T, N>& buf) : ArrayQueue(buf.data(), N) {}
@@ -36,7 +39,9 @@ template <typename T>
 ArrayQueue<T>::ArrayQueue(T* buf, size_t size)
   : data_{buf}, read_pos_{0}, write_pos_{0}, count_{0}, capacity_{size}
 {}
+// #@@range_end(constructor)
 
+// #@@range_begin(push)
 template <typename T>
 Error ArrayQueue<T>::Push(const T& value) {
   if (count_ == capacity_) {
@@ -51,7 +56,9 @@ Error ArrayQueue<T>::Push(const T& value) {
   }
   return MAKE_ERROR(Error::kSuccess);
 }
+// #@@range_end(push)
 
+// #@@range_begin(pop)
 template <typename T>
 Error ArrayQueue<T>::Pop() {
   if (count_ == 0) {
@@ -65,6 +72,7 @@ Error ArrayQueue<T>::Pop() {
   }
   return MAKE_ERROR(Error::kSuccess);
 }
+// #@@range_end(pop)
 
 template <typename T>
 size_t ArrayQueue<T>::Count() const {
@@ -76,7 +84,9 @@ size_t ArrayQueue<T>::Capacity() const {
   return capacity_;
 }
 
+// #@@range_begin(front)
 template <typename T>
 const T& ArrayQueue<T>::Front() const {
   return data_[read_pos_];
 }
+// #@@range_end(front)
