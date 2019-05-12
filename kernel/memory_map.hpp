b7d7cfcca9d5542467b8b08b3a80847dafb1a34c
uchan-nos <uchan0@gmail.com>
2019-05-12 16:58:48 +0900

    pass memory map to the kernel

---
+++ kernel/memory_map.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <stdint.h>
+
+struct MemoryMap {
+  unsigned long long buffer_size;
+  void* buffer;
+  unsigned long long map_size;
+  unsigned long long map_key;
+  unsigned long long descriptor_size;
+  uint32_t descriptor_version;
+};
+
+struct MemoryDescriptor {
+  uint32_t type;
+  uintptr_t physical_start;
+  uintptr_t virtual_start;
+  uint64_t number_of_pages;
+  uint64_t attribute;
+};
+
+#ifdef __cplusplus
+enum class MemoryType {
+  kEfiReservedMemoryType,
+  kEfiLoaderCode,
+  kEfiLoaderData,
+  kEfiBootServicesCode,
+  kEfiBootServicesData,
+  kEfiRuntimeServicesCode,
+  kEfiRuntimeServicesData,
+  kEfiConventionalMemory,
+  kEfiUnusableMemory,
+  kEfiACPIReclaimMemory,
+  kEfiACPIMemoryNVS,
+  kEfiMemoryMappedIO,
+  kEfiMemoryMappedIOPortSpace,
+  kEfiPalCode,
+  kEfiPersistentMemory,
+  kEfiMaxMemoryType
+};
+
+inline bool operator==(uint32_t lhs, MemoryType rhs) {
+  return lhs == static_cast<uint32_t>(rhs);
+}
+
+inline bool operator==(MemoryType lhs, uint32_t rhs) {
+  return rhs == lhs;
+}
+#endif
