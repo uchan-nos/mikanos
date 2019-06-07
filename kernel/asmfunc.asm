739285b2f56735fed06cb34199f3ae154745ba38
uchan-nos <uchan0@gmail.com>
2019-06-07 15:46:33 +0900

    Setup GDT and paging

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -36,3 +36,47 @@ LoadIDT:
     mov rsp, rbp
     pop rbp
     ret
+
+global LoadGDT  ; void LoadGDT(uint16_t limit, uint64_t offset);
+LoadGDT:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 10
+    mov [rsp], di  ; limit
+    mov [rsp + 2], rsi  ; offset
+    lgdt [rsp]
+    mov rsp, rbp
+    pop rbp
+    ret
+
+global SetCS  ; void SetCS(uint16_t value);
+SetCS:
+    push rbp
+    mov rbp, rsp
+    xor eax, eax
+    mov ax, ss
+    mov rcx, .next
+    push rax    ; SS
+    push rbp    ; RSP
+    pushfq      ; RFLAGS
+    push rdi    ; CS
+    push rcx    ; RIP
+    iretq
+.next:
+    mov rsp, rbp
+    pop rbp
+    ret
+
+global SetDSAll  ; void SetDSAll(uint16_t value);
+SetDSAll:
+    mov ds, di
+    mov es, di
+    mov ss, di
+    mov fs, di
+    mov gs, di
+    ret
+
+global SetCR3  ; void SetCR3(uint64_t value);
+SetCR3:
+    mov cr3, rdi
+    ret
