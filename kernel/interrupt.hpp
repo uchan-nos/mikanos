1cb1b2b866d24086d4906cf93f2ab3c8cea4f82e
uchan-nos <uchan0@gmail.com>
2019-06-07 15:46:33 +0900

    Setup GDT and paging

--- kernel/interrupt.hpp
+++ kernel/interrupt.hpp
@@ -9,15 +9,7 @@
 #include <array>
 #include <cstdint>
 
-enum class DescriptorType {
-  kUpper8Bytes   = 0,
-  kLDT           = 2,
-  kTSSAvailable  = 9,
-  kTSSBusy       = 11,
-  kCallGate      = 12,
-  kInterruptGate = 14,
-  kTrapGate      = 15,
-};
+#include "x86_descriptor.hpp"
 
 union InterruptDescriptorAttribute {
   uint16_t data;
