0e18d55fca81bd6f220eaf9c410e0b7073d83dfa
uchan-nos <uchan0@gmail.com>
2019-06-25 21:25:17 +0900

    fix bug for multiple usb device
    - to initialize each root hub port one by one
    - to emit SET_PROTOCOL before receiving data from an HID
    - refactoring

--- kernel/usb/xhci/device.hpp
+++ kernel/usb/xhci/device.hpp
@@ -11,46 +11,12 @@
 
 #include "error.hpp"
 #include "usb/device.hpp"
+#include "usb/arraymap.hpp"
 #include "usb/xhci/context.hpp"
 #include "usb/xhci/trb.hpp"
 #include "usb/xhci/registers.hpp"
 
 namespace usb::xhci {
-  class SetupStageMap {
-   public:
-    const SetupStageTRB* Get(const void* key) const {
-      for (int i = 0; i < table_.size(); ++i) {
-        if (table_[i].first == key) {
-          return table_[i].second;
-        }
-      }
-      return nullptr;
-    }
-
-    void Put(const void* key, const SetupStageTRB* value) {
-      for (int i = 0; i < table_.size(); ++i) {
-        if (table_[i].first == nullptr) {
-          table_[i].first = key;
-          table_[i].second = value;
-          break;
-        }
-      }
-    }
-
-    void Delete(const void* key) {
-      for (int i = 0; i < table_.size(); ++i) {
-        if (table_[i].first == key) {
-          table_[i].first = nullptr;
-          table_[i].second = nullptr;
-          break;
-        }
-      }
-    }
-
-   private:
-    std::array<std::pair<const void*, const SetupStageTRB*>, 16> table_{};
-  };
-
   class Device : public usb::Device {
    public:
     enum class State {
@@ -82,10 +48,12 @@ namespace usb::xhci {
     void SelectForSlotAssignment();
     Ring* AllocTransferRing(DeviceContextIndex index, size_t buf_size);
 
-    Error ControlIn(int ep_num, SetupData setup_data, void* buf, int len) override;
-    Error ControlOut(int ep_num, SetupData setup_data, const void* buf, int len) override;
-    Error InterruptIn(int ep_num, void* buf, int len) override;
-    Error InterruptOut(int ep_num, void* buf, int len) override;
+    Error ControlIn(EndpointID ep_id, SetupData setup_data,
+                    void* buf, int len, ClassDriver* issuer) override;
+    Error ControlOut(EndpointID ep_id, SetupData setup_data,
+                     const void* buf, int len, ClassDriver* issuer) override;
+    Error InterruptIn(EndpointID ep_id, void* buf, int len) override;
+    Error InterruptOut(EndpointID ep_id, void* buf, int len) override;
 
     Error OnTransferEventReceived(const TransferEventTRB& trb);
 
@@ -99,7 +67,10 @@ namespace usb::xhci {
     enum State state_;
     std::array<Ring*, 31> transfer_rings_; // index = dci - 1
 
-    SetupStageMap setup_stage_map_{};
+    /** コントロール転送が完了した際に DataStageTRB や StatusStageTRB
+     * から対応する SetupStageTRB を検索するためのマップ．
+     */
+    ArrayMap<const void*, const SetupStageTRB*, 16> setup_stage_map_{};
 
     //usb::Device* usb_device_;
   };
