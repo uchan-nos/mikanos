f4dccf6dc5972dd7046a8a600617dab94c0f2014
uchan-nos <uchan0@gmail.com>
2019-06-25 21:25:17 +0900

    fix bug for multiple usb device
    - to initialize each root hub port one by one
    - to emit SET_PROTOCOL before receiving data from an HID
    - refactoring

--- kernel/usb/classdriver/base.hpp
+++ kernel/usb/classdriver/base.hpp
@@ -21,12 +21,9 @@ namespace usb {
     virtual Error Initialize() = 0;
     virtual Error SetEndpoint(const EndpointConfig& config) = 0;
     virtual Error OnEndpointsConfigured() = 0;
-    virtual Error OnControlOutCompleted(SetupData setup_data,
-                                        const void* buf, int len) = 0;
-    virtual Error OnControlInCompleted(SetupData setup_data,
-                                       const void* buf, int len) = 0;
-    virtual Error OnInterruptOutCompleted(const void* buf, int len) = 0;
-    virtual Error OnInterruptInCompleted(const void* buf, int len) = 0;
+    virtual Error OnControlCompleted(EndpointID ep_id, SetupData setup_data,
+                                     const void* buf, int len) = 0;
+    virtual Error OnInterruptCompleted(EndpointID ep_id, const void* buf, int len) = 0;
 
     /** このクラスドライバを保持する USB デバイスを返す． */
     Device* ParentDevice() const { return dev_; }
