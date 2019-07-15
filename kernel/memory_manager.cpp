64148b63e0f6a661046b5eaff06a7253ef7a7840
uchan-nos <uchan0@gmail.com>
2019-07-15 17:42:14 +0900

    Revert "add review tags"
    This reverts commit 4b4983397d68bca31d607ccbbc59f3c5d4b3f18e.

--- kernel/memory_manager.cpp
+++ kernel/memory_manager.cpp
@@ -4,7 +4,6 @@ BitmapMemoryManager::BitmapMemoryManager()
   : alloc_map_{}, range_begin_{FrameID{0}}, range_end_{FrameID{kFrameCount}} {
 }
 
-// #@@range_begin(allocate)
 WithError<FrameID> BitmapMemoryManager::Allocate(size_t num_frames) {
   size_t start_frame_id = range_begin_.ID();
   while (true) {
@@ -30,33 +29,25 @@ WithError<FrameID> BitmapMemoryManager::Allocate(size_t num_frames) {
     start_frame_id += i + 1;
   }
 }
-// #@@range_end(allocate)
 
-// #@@range_begin(free)
 Error BitmapMemoryManager::Free(FrameID start_frame, size_t num_frames) {
   for (size_t i = 0; i < num_frames; ++i) {
     SetBit(FrameID{start_frame.ID() + i}, false);
   }
   return MAKE_ERROR(Error::kSuccess);
 }
-// #@@range_end(free)
 
-// #@@range_begin(mark_allocated)
 void BitmapMemoryManager::MarkAllocated(FrameID start_frame, size_t num_frames) {
   for (size_t i = 0; i < num_frames; ++i) {
     SetBit(FrameID{start_frame.ID() + i}, true);
   }
 }
-// #@@range_end(mark_allocated)
 
-// #@@range_begin(set_memory_range)
 void BitmapMemoryManager::SetMemoryRange(FrameID range_begin, FrameID range_end) {
   range_begin_ = range_begin;
   range_end_ = range_end;
 }
-// #@@range_end(set_memory_range)
 
-// #@@range_begin(get_set_bit)
 bool BitmapMemoryManager::GetBit(FrameID frame) const {
   auto line_index = frame.ID() / kBitsPerMapLine;
   auto bit_index = frame.ID() % kBitsPerMapLine;
@@ -74,4 +65,3 @@ void BitmapMemoryManager::SetBit(FrameID frame, bool allocated) {
     alloc_map_[line_index] &= ~(static_cast<MapLineType>(1) << bit_index);
   }
 }
-// #@@range_end(get_set_bit)
