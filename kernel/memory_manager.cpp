dac2f6b237fdb82e8fdd9fd28a8253f3c92cb26e
uchan-nos <uchan0@gmail.com>
2019-07-17 11:32:56 +0900

    implement sbrk to use allocated pages

--- kernel/memory_manager.cpp
+++ kernel/memory_manager.cpp
@@ -65,3 +65,17 @@ void BitmapMemoryManager::SetBit(FrameID frame, bool allocated) {
     alloc_map_[line_index] &= ~(static_cast<MapLineType>(1) << bit_index);
   }
 }
+
+extern "C" caddr_t program_break, program_break_end;
+
+Error InitializeHeap(BitmapMemoryManager& memory_manager) {
+  const int kHeapFrames = 512;
+  const auto heap_start = memory_manager.Allocate(kHeapFrames);
+  if (heap_start.error) {
+    return heap_start.error;
+  }
+
+  program_break = reinterpret_cast<caddr_t>(heap_start.value.ID() * kBytesPerFrame);
+  program_break_end = program_break + kHeapFrames * kBytesPerFrame;
+  return MAKE_ERROR(Error::kSuccess);
+}
