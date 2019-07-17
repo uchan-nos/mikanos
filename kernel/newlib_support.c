5aa25063d9dd1c3a6e544ccc91c7a8f949d3814c
uchan-nos <uchan0@gmail.com>
2019-07-17 14:15:35 +0900

    add newlib support functions

--- kernel/newlib_support.c
+++ kernel/newlib_support.c
@@ -1,7 +1,20 @@
 #include <errno.h>
 #include <sys/types.h>
 
+void _exit(void) {
+  while (1) __asm__("hlt");
+}
+
 caddr_t sbrk(int incr) {
   errno = ENOMEM;
   return (caddr_t)-1;
+}
+
+int getpid(void) {
+  return 1;
+}
+
+int kill(int pid, int sig) {
+  errno = EINVAL;
+  return -1;
 }
