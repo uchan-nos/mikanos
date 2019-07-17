a1e67673cca4619413139830d04d2a4e9ac97fdf
uchan-nos <uchan0@gmail.com>
2019-07-17 11:32:56 +0900

    implement sbrk to use allocated pages

--- kernel/newlib_support.c
+++ kernel/newlib_support.c
@@ -5,9 +5,17 @@ void _exit(void) {
   while (1) __asm__("hlt");
 }
 
+caddr_t program_break, program_break_end;
+
 caddr_t sbrk(int incr) {
-  errno = ENOMEM;
-  return (caddr_t)-1;
+  if (program_break == 0 || program_break + incr >= program_break_end) {
+    errno = ENOMEM;
+    return (caddr_t)-1;
+  }
+
+  caddr_t prev_break = program_break;
+  program_break += incr;
+  return prev_break;
 }
 
 int getpid(void) {
