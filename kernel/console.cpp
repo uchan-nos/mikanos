228c0051122127e5b5540b1c3956d769687ad96f
uchan-nos <uchan0@gmail.com>
2019-07-24 06:13:10 +0900

    use layer for mouse and desktop

--- kernel/console.cpp
+++ kernel/console.cpp
@@ -9,10 +9,17 @@
 #include <cstring>
 #include "font.hpp"
 
-Console::Console(PixelWriter& writer,
-    const PixelColor& fg_color, const PixelColor& bg_color)
-    : writer_{writer}, fg_color_{fg_color}, bg_color_{bg_color},
+Console::Console(const PixelColor& fg_color, const PixelColor& bg_color)
+    : writer_{nullptr}, fg_color_{fg_color}, bg_color_{bg_color},
       buffer_{}, cursor_row_{0}, cursor_column_{0} {
+}
+
+void Console::SetWriter(PixelWriter* writer) {
+  if (writer == writer_) {
+    return;
+  }
+  writer_ = writer;
+  Refresh();
 }
 
 void Console::PutString(const char* s) {
@@ -20,7 +27,7 @@ void Console::PutString(const char* s) {
     if (*s == '\n') {
       Newline();
     } else if (cursor_column_ < kColumns - 1) {
-      WriteAscii(writer_, 8 * cursor_column_, 16 * cursor_row_, *s, fg_color_);
+      WriteAscii(*writer_, 8 * cursor_column_, 16 * cursor_row_, *s, fg_color_);
       buffer_[cursor_row_][cursor_column_] = *s;
       ++cursor_column_;
     }
@@ -35,13 +42,19 @@ void Console::Newline() {
   } else {
     for (int y = 0; y < 16 * kRows; ++y) {
       for (int x = 0; x < 8 * kColumns; ++x) {
-        writer_.Write(x, y, bg_color_);
+        writer_->Write(x, y, bg_color_);
       }
     }
     for (int row = 0; row < kRows - 1; ++row) {
       memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
-      WriteString(writer_, 0, 16 * row, buffer_[row], fg_color_);
+      WriteString(*writer_, 0, 16 * row, buffer_[row], fg_color_);
     }
     memset(buffer_[kRows - 1], 0, kColumns + 1);
+  }
+}
+
+void Console::Refresh() {
+  for (int row = 0; row < kRows; ++row) {
+    WriteString(*writer_, 0, 16 * row, buffer_[row], fg_color_);
   }
 }
