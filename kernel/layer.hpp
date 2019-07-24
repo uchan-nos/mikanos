6e02d7824c05c31ad3bba17505d1c57a688a8213
uchan-nos <uchan0@gmail.com>
2019-07-25 06:16:16 +0900

    window DrawTo receive top-left position instead of center
    and refactor & comment code

--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -5,40 +5,21 @@
 #include <vector>
 
 #include "graphics.hpp"
+#include "window.hpp"
 
 class Layer {
  public:
-  Layer(unsigned int id = 0) : id_{id} {
-  }
+  Layer(unsigned int id = 0);
 
-  unsigned int ID() const {
-    return id_;
-  }
+  unsigned int ID() const;
 
-  Layer& SetWindow(const std::shared_ptr<Window>& window) {
-    window_ = window;
-    return *this;
-  }
+  Layer& SetWindow(const std::shared_ptr<Window>& window);
+  std::shared_ptr<Window> GetWindow() const;
 
-  std::shared_ptr<Window> GetWindow() const {
-    return window_;
-  }
+  Layer& Move(Vector2D<int> pos);
+  Layer& MoveRelative(Vector2D<int> pos_diff);
 
-  Layer& Move(Vector2D<int> pos) {
-    pos_ = pos;
-    return *this;
-  }
-
-  Layer& MoveRelative(Vector2D<int> pos_diff) {
-    pos_ += pos_diff;
-    return *this;
-  }
-
-  void DrawTo(PixelWriter& writer) const {
-    if (window_) {
-      window_->DrawTo(writer, pos_);
-    }
-  }
+  void DrawTo(PixelWriter& writer) const;
 
  private:
   unsigned int id_;
@@ -48,70 +29,18 @@ class Layer {
 
 class LayerManager {
  public:
-  void SetWriter(PixelWriter* writer) {
-    writer_ = writer;
-  }
+  void SetWriter(PixelWriter* writer);
 
-  Layer& NewLayer() {
-    ++latest_id_;
-    auto [ it, _ ] = layers_.emplace(latest_id_, new Layer{latest_id_});
-    return *it->second;
-  }
+  Layer& NewLayer();
 
-  void Draw() const {
-    for (auto layer : layer_stack_) {
-      layer->DrawTo(*writer_);
-    }
-  }
+  void Draw() const;
 
-  void Move(unsigned int id, Vector2D<int> new_position) {
-    layers_[id]->Move(new_position);
-    Draw();
-  }
+  void Move(unsigned int id, Vector2D<int> new_position);
+  void MoveRelative(unsigned int id, Vector2D<int> pos_diff);
 
-  void MoveRelative(unsigned int id, Vector2D<int> pos_diff) {
-    layers_[id]->MoveRelative(pos_diff);
-    Draw();
-  }
-
-  void UpDown(unsigned int id, int new_height) {
-    if (new_height < 0) {
-      Hide(id);
-      return;
-    }
-    if (new_height > layer_stack_.size()) {
-      new_height = layer_stack_.size();
-    }
-
-    auto layer = layers_[id].get();
-    auto new_pos = layer_stack_.begin() + new_height;
-    auto old_pos = std::find(layer_stack_.begin(), layer_stack_.end(), layer);
-    if (old_pos == layer_stack_.end()) {
-      layer_stack_.insert(new_pos, layer);
-      return;
-    }
-    if (old_pos == new_pos) {
-      return;
-    }
-
-    if (old_pos < new_pos) {
-      --new_pos;
-    }
-    layer_stack_.erase(old_pos);
-    layer_stack_.insert(new_pos, layer);
-  }
-
-  void Hide(unsigned int id) {
-    auto layer = layers_[id].get();
-    auto pos = std::find(layer_stack_.begin(), layer_stack_.end(), layer);
-    if (pos != layer_stack_.end()) {
-      layer_stack_.erase(pos);
-    }
-  }
-
-  void Topmost(unsigned int id) {
-    UpDown(id, layer_stack_.size());
-  }
+  void UpDown(unsigned int id, int new_height);
+  void Hide(unsigned int id);
+  void Topmost(unsigned int id);
 
  private:
   PixelWriter* writer_{nullptr};
