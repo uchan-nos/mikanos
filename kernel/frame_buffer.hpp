65d6846d2843c217bb2ad7ad0f37696441b771f8
uchan-nos <uchan0@gmail.com>
2019-08-04 19:39:03 +0900

    add FrameBuffer, optimize re-draw

---
+++ kernel/frame_buffer.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include "frame_buffer_config.hpp"
+#include "graphics.hpp"
+#include "error.hpp"
+
+class FrameBuffer {
+ public:
+  Error Initialize(const FrameBufferConfig& config);
+  Error Copy(int x, int y, const FrameBuffer& src);
+
+  FrameBufferWriter& Writer() { return *writer_; }
+
+ private:
+  FrameBufferConfig config_{};
+  std::vector<uint8_t> buffer_{};
+  std::unique_ptr<FrameBufferWriter> writer_{};
+};
+
+int BitsPerPixel(PixelFormat format);
--- kernel/libcxx_support.cpp
+++ kernel/libcxx_support.cpp
@@ -1,8 +1,13 @@
 #include <new>
 #include <cerrno>
 
+int printk(const char* format, ...);
+
 std::new_handler std::get_new_handler() noexcept {
-  return nullptr;
+  return [] {
+    printk("not enough memory\n");
+    exit(1);
+  };
 }
 
 extern "C" int posix_memalign(void**, size_t, size_t) {
