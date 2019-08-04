51c88fc35c008b188115334913c11685d8abee0e
uchan-nos <uchan0@gmail.com>
2019-08-04 21:20:40 +0900

    refactor to use Vector2D

--- kernel/font.hpp
+++ kernel/font.hpp
@@ -3,5 +3,5 @@
 #include <cstdint>
 #include "graphics.hpp"
 
-void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color);
-void WriteString(PixelWriter& writer, int x, int y, const char* s, const PixelColor& color);
+void WriteAscii(PixelWriter& writer, Vector2D<int> pos, char c, const PixelColor& color);
+void WriteString(PixelWriter& writer, Vector2D<int> pos, const char* s, const PixelColor& color);
