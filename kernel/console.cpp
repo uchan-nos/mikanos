42a4d60233dbc8d1a8b604374d089e3d3b60c2c0
uchan-nos <uchan0@gmail.com>
2019-08-04 21:20:40 +0900

    refactor to use Vector2D

--- kernel/console.cpp
+++ kernel/console.cpp
@@ -20,7 +20,7 @@ void Console::PutString(const char* s) {
     if (*s == '\n') {
       Newline();
     } else if (cursor_column_ < kColumns - 1) {
-      WriteAscii(*writer_, 8 * cursor_column_, 16 * cursor_row_, *s, fg_color_);
+      WriteAscii(*writer_, Vector2D<int>{8 * cursor_column_, 16 * cursor_row_}, *s, fg_color_);
       buffer_[cursor_row_][cursor_column_] = *s;
       ++cursor_column_;
     }
@@ -46,12 +46,12 @@ void Console::Newline() {
   } else {
     for (int y = 0; y < 16 * kRows; ++y) {
       for (int x = 0; x < 8 * kColumns; ++x) {
-        writer_->Write(x, y, bg_color_);
+        writer_->Write(Vector2D<int>{x, y}, bg_color_);
       }
     }
     for (int row = 0; row < kRows - 1; ++row) {
       memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
-      WriteString(*writer_, 0, 16 * row, buffer_[row], fg_color_);
+      WriteString(*writer_, Vector2D<int>{0, 16 * row}, buffer_[row], fg_color_);
     }
     memset(buffer_[kRows - 1], 0, kColumns + 1);
   }
@@ -59,6 +59,6 @@ void Console::Newline() {
 
 void Console::Refresh() {
   for (int row = 0; row < kRows; ++row) {
-    WriteString(*writer_, 0, 16 * row, buffer_[row], fg_color_);
+    WriteString(*writer_, Vector2D<int>{0, 16 * row}, buffer_[row], fg_color_);
   }
 }
