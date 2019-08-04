849262bda9de5e40e20876c5d24e057355ca7b74
uchan-nos <uchan0@gmail.com>
2019-08-04 19:39:03 +0900

    add FrameBuffer, optimize re-draw

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -28,15 +28,15 @@ Layer& Layer::MoveRelative(Vector2D<int> pos_diff) {
   return *this;
 }
 
-void Layer::DrawTo(PixelWriter& writer) const {
+void Layer::DrawTo(FrameBuffer& screen) const {
   if (window_) {
-    window_->DrawTo(writer, pos_);
+    window_->DrawTo(screen, pos_);
   }
 }
 
 
-void LayerManager::SetWriter(PixelWriter* writer) {
-  writer_ = writer;
+void LayerManager::SetWriter(FrameBuffer* screen) {
+  screen_ = screen;
 }
 
 Layer& LayerManager::NewLayer() {
@@ -46,7 +46,7 @@ Layer& LayerManager::NewLayer() {
 
 void LayerManager::Draw() const {
   for (auto layer : layer_stack_) {
-    layer->DrawTo(*writer_);
+    layer->DrawTo(*screen_);
   }
 }
 
