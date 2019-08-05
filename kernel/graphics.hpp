d354c701bbdd9921737ca185d6798024bb7fde8b
uchan-nos <uchan0@gmail.com>
2019-08-05 19:56:13 +0900

    refactor

--- kernel/graphics.hpp
+++ kernel/graphics.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include "frame_buffer_config.hpp"
 
 struct PixelColor {
@@ -30,12 +31,16 @@ template <typename T, typename U>
 auto operator +(const Vector2D<T>& lhs, const Vector2D<U>& rhs)
     -> Vector2D<decltype(lhs.x + rhs.x)> {
   return {lhs.x + rhs.x, lhs.y + rhs.y};
-  /*
-  auto tmp{lhs};
-  tmp.x += rhs.x;
-  tmp.y += rhs.y;
-  return tmp;
-  */
+}
+
+template <typename T>
+Vector2D<T> ElementMax(const Vector2D<T>& lhs, const Vector2D<T>& rhs) {
+  return {std::max(lhs.x, rhs.x), std::max(lhs.y, rhs.y)};
+}
+
+template <typename T>
+Vector2D<T> ElementMin(const Vector2D<T>& lhs, const Vector2D<T>& rhs) {
+  return {std::min(lhs.x, rhs.x), std::min(lhs.y, rhs.y)};
 }
 
 class PixelWriter {
