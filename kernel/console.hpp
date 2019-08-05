aea32bd13b1f50ed0c1bba9b55124d98069023b1
uchan-nos <uchan0@gmail.com>
2019-08-05 10:22:35 +0900

    implement move method to frame buffer

--- kernel/console.hpp
+++ kernel/console.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <memory>
 #include "graphics.hpp"
+#include "window.hpp"
 
 class Console {
  public:
@@ -9,12 +11,14 @@ class Console {
   Console(const PixelColor& fg_color, const PixelColor& bg_color);
   void PutString(const char* s);
   void SetWriter(PixelWriter* writer);
+  void SetWindow(const std::shared_ptr<Window>& window);
 
  private:
   void Newline();
   void Refresh();
 
   PixelWriter* writer_;
+  std::shared_ptr<Window> window_;
   const PixelColor fg_color_, bg_color_;
   char buffer_[kRows][kColumns + 1];
   int cursor_row_, cursor_column_;
