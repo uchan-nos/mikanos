eab3aacf9696dba3eed6c0c1a2c7f03e3faea7f3
uchan-nos <uchan0@gmail.com>
2019-08-06 15:56:45 +0900

    re-draw limited area

--- kernel/frame_buffer.hpp
+++ kernel/frame_buffer.hpp
@@ -10,7 +10,7 @@
 class FrameBuffer {
  public:
   Error Initialize(const FrameBufferConfig& config);
-  Error Copy(Vector2D<int> dst_pos, const FrameBuffer& src);
+  Error Copy(Vector2D<int> dst_pos, const FrameBuffer& src, const Rectangle<int>& src_area);
   void Move(Vector2D<int> dst_pos, const Rectangle<int>& src);
 
   FrameBufferWriter& Writer() { return *writer_; }
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -65,7 +65,6 @@ void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
   mouse_position = ElementMax(newpos, {0, 0});
 
   layer_manager->Move(mouse_layer_id, mouse_position);
-  layer_manager->Draw();
 }
 
 void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
@@ -261,7 +260,6 @@ extern "C" void KernelMainNewStack(
   auto bgwriter = bgwindow->Writer();
 
   DrawDesktop(*bgwriter);
-  console->SetWindow(bgwindow);
 
   auto mouse_window = std::make_shared<Window>(
       kMouseCursorWidth, kMouseCursorHeight, frame_buffer_config.pixel_format);
@@ -272,6 +270,10 @@ extern "C" void KernelMainNewStack(
   auto main_window = std::make_shared<Window>(
       160, 52, frame_buffer_config.pixel_format);
   DrawWindow(*main_window->Writer(), "Hello Window");
+
+  auto console_window = std::make_shared<Window>(
+      Console::kColumns * 8, Console::kRows * 16, frame_buffer_config.pixel_format);
+  console->SetWindow(console_window);
 
   FrameBuffer screen;
   if (auto err = screen.Initialize(frame_buffer_config)) {
@@ -294,11 +296,16 @@ extern "C" void KernelMainNewStack(
     .SetWindow(main_window)
     .Move({300, 100})
     .ID();
+  console->SetLayerID(layer_manager->NewLayer()
+    .SetWindow(console_window)
+    .Move({0, 0})
+    .ID());
 
   layer_manager->UpDown(bglayer_id, 0);
-  layer_manager->UpDown(mouse_layer_id, 1);
-  layer_manager->UpDown(main_window_layer_id, 1);
-  layer_manager->Draw();
+  layer_manager->UpDown(console->LayerID(), 1);
+  layer_manager->UpDown(main_window_layer_id, 2);
+  layer_manager->UpDown(mouse_layer_id, 3);
+  layer_manager->Draw({{0, 0}, screen_size});
 
   char str[128];
   unsigned int count = 0;
@@ -308,7 +315,7 @@ extern "C" void KernelMainNewStack(
     sprintf(str, "%010u", count);
     FillRectangle(*main_window->Writer(), {24, 28}, {8 * 10, 16}, {0xc6, 0xc6, 0xc6});
     WriteString(*main_window->Writer(), {24, 28}, str, {0, 0, 0});
-    layer_manager->Draw();
+    layer_manager->Draw(main_window_layer_id);
 
     __asm__("cli");
     if (main_queue.Count() == 0) {
