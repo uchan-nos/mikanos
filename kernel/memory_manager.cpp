9d941e378961f55373629a5b70fa0db464db5dd7
uchan-nos <uchan0@gmail.com>
2019-08-07 10:31:59 +0900

    increase heap to 128MiB

--- kernel/memory_manager.cpp
+++ kernel/memory_manager.cpp
@@ -70,7 +70,7 @@ void BitmapMemoryManager::SetBit(FrameID frame, bool allocated) {
 extern "C" caddr_t program_break, program_break_end;
 
 Error InitializeHeap(BitmapMemoryManager& memory_manager) {
-  const int kHeapFrames = 512;
+  const int kHeapFrames = 64 * 512;
   const auto heap_start = memory_manager.Allocate(kHeapFrames);
   if (heap_start.error) {
     return heap_start.error;
