c8819778e6d3b074396ab9cb53da02054d99bb08
uchan-nos <uchan0@gmail.com>
2019-08-12 18:10:23 +0900

    measure console scroll time

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -50,6 +50,12 @@ int printk(const char* format, ...) {
   result = vsprintf(s, format, ap);
   va_end(ap);
 
+  StartLAPICTimer();
+  console->PutString(s);
+  auto elapsed = LAPICTimerElapsed();
+  StopLAPICTimer();
+
+  sprintf(s, "[%9d]", elapsed);
   console->PutString(s);
   return result;
 }
