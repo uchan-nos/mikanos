ce505bb7d7888bc9d0a0c655729ea8eab2e9e34c
uchan-nos <uchan0@gmail.com>
2019-08-18 15:33:39 +0900

    use FillRectangle

--- kernel/console.cpp
+++ kernel/console.cpp
@@ -62,24 +62,15 @@ void Console::Newline() {
   if (window_) {
     Rectangle<int> move_src{{0, 16}, {8 * kColumns, 16 * (kRows - 1)}};
     window_->Move({0, 0}, move_src);
-    for (int y = 16 * (kRows - 1); y < 16 * kRows; ++y) {
-      for (int x = 0; x < 8 * kColumns; ++x) {
-        writer_->Write(Vector2D<int>{x, y}, bg_color_);
-      }
-    }
+    FillRectangle(*writer_, {0, 16 * (kRows - 1)}, {8 * kColumns, 16}, bg_color_);
   } else {
-    for (int y = 0; y < 16 * kRows; ++y) {
-      for (int x = 0; x < 8 * kColumns; ++x) {
-        writer_->Write(Vector2D<int>{x, y}, bg_color_);
-      }
-    }
+    FillRectangle(*writer_, {0, 0}, {8 * kColumns, 16 * kRows}, bg_color_);
     for (int row = 0; row < kRows - 1; ++row) {
       memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
       WriteString(*writer_, Vector2D<int>{0, 16 * row}, buffer_[row], fg_color_);
     }
+    memset(buffer_[kRows - 1], 0, kColumns + 1);
   }
-
-  memset(buffer_[kRows - 1], 0, kColumns + 1);
 }
 // #@@range_end(newline)
 
