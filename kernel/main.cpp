023a4d9443960e7df7cbc3fb0bf289e2b38acdda
uchan-nos <uchan0@gmail.com>
2019-08-20 17:18:08 +0900

    make one window widget

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -269,6 +269,12 @@ extern "C" void KernelMainNewStack(
   DrawMouseCursor(mouse_window->Writer(), {0, 0});
   mouse_position = {200, 200};
 
+  auto main_window = std::make_shared<Window>(
+      160, 68, frame_buffer_config.pixel_format);
+  DrawWindow(*main_window->Writer(), "Hello Window");
+  WriteString(*main_window->Writer(), {24, 28}, "Welcome to", {0, 0, 0});
+  WriteString(*main_window->Writer(), {24, 44}, " MikanOS world!", {0, 0, 0});
+
   FrameBuffer screen;
   if (auto err = screen.Initialize(frame_buffer_config)) {
     Log(kError, "failed to initialize frame buffer: %s at %s:%d\n",
@@ -286,9 +292,14 @@ extern "C" void KernelMainNewStack(
     .SetWindow(mouse_window)
     .Move(mouse_position)
     .ID();
+  auto main_window_layer_id = layer_manager->NewLayer()
+    .SetWindow(main_window)
+    .Move({300, 100})
+    .ID();
 
   layer_manager->UpDown(bglayer_id, 0);
   layer_manager->UpDown(mouse_layer_id, 1);
+  layer_manager->UpDown(main_window_layer_id, 1);
   layer_manager->Draw();
 
   while (true) {
--- kernel/window.cpp
+++ kernel/window.cpp
@@ -1,6 +1,7 @@
 #include "window.hpp"
 
 #include "logger.hpp"
+#include "font.hpp"
 
 Window::Window(int width, int height, PixelFormat shadow_format) : width_{width}, height_{height} {
   data_.resize(height);
@@ -69,4 +70,69 @@ int Window::Height() const {
 
 void Window::Move(Vector2D<int> dst_pos, const Rectangle<int>& src) {
   shadow_buffer_.Move(dst_pos, src);
+}
+
+
+namespace {
+  const int kCloseButtonWidth = 16;
+  const int kCloseButtonHeight = 14;
+  const char close_button[kCloseButtonHeight][kCloseButtonWidth + 1] = {
+    "...............@",
+    ".:::::::::::::$@",
+    ".:::::::::::::$@",
+    ".:::@@::::@@::$@",
+    ".::::@@::@@:::$@",
+    ".:::::@@@@::::$@",
+    ".::::::@@:::::$@",
+    ".:::::@@@@::::$@",
+    ".::::@@::@@:::$@",
+    ".:::@@::::@@::$@",
+    ".:::::::::::::$@",
+    ".:::::::::::::$@",
+    ".$$$$$$$$$$$$$$@",
+    "@@@@@@@@@@@@@@@@",
+  };
+
+  constexpr PixelColor ToColor(uint32_t c) {
+    return {
+      static_cast<uint8_t>((c >> 16) & 0xff),
+      static_cast<uint8_t>((c >> 8) & 0xff),
+      static_cast<uint8_t>(c & 0xff)
+    };
+  }
+}
+
+void DrawWindow(PixelWriter& writer, const char* title) {
+  auto fill_rect = [&writer](Vector2D<int> pos, Vector2D<int> size, uint32_t c) {
+    FillRectangle(writer, pos, size, ToColor(c));
+  };
+  const auto win_w = writer.Width();
+  const auto win_h = writer.Height();
+
+  fill_rect({0, 0},         {win_w, 1},             0xc6c6c6);
+  fill_rect({1, 1},         {win_w - 2, 1},         0xffffff);
+  fill_rect({0, 0},         {1, win_h},             0xc6c6c6);
+  fill_rect({1, 1},         {1, win_h - 2},         0xffffff);
+  fill_rect({win_w - 2, 1}, {1, win_h - 2},         0x848484);
+  fill_rect({win_w - 1, 0}, {1, win_h},             0x000000);
+  fill_rect({2, 2},         {win_w - 4, win_h - 4}, 0xc6c6c6);
+  fill_rect({3, 3},         {win_w - 6, 18},        0x000084);
+  fill_rect({1, win_h - 2}, {win_w - 2, 1},         0x848484);
+  fill_rect({0, win_h - 1}, {win_w, 1},             0x000000);
+
+  WriteString(writer, {24, 4}, title, ToColor(0xffffff));
+
+  for (int y = 0; y < kCloseButtonHeight; ++y) {
+    for (int x = 0; x < kCloseButtonWidth; ++x) {
+      PixelColor c = ToColor(0xffffff);
+      if (close_button[y][x] == '@') {
+        c = ToColor(0x000000);
+      } else if (close_button[y][x] == '$') {
+        c = ToColor(0x848484);
+      } else if (close_button[y][x] == ':') {
+        c = ToColor(0xc6c6c6);
+      }
+      writer.Write({win_w - 5 - kCloseButtonWidth + x, 5 + y}, c);
+    }
+  }
 }
