94595488d852c86c8b1368b593abcdf031864e56
uchan-nos <uchan0@gmail.com>
2019-08-23 08:33:05 +0900

    draw via shadow buffer

--- kernel/frame_buffer.hpp
+++ kernel/frame_buffer.hpp
@@ -14,6 +14,7 @@ class FrameBuffer {
   void Move(Vector2D<int> dst_pos, const Rectangle<int>& src);
 
   FrameBufferWriter& Writer() { return *writer_; }
+  const FrameBufferConfig& Config() const { return config_; }
 
  private:
   FrameBufferConfig config_{};
