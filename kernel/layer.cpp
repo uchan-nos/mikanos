60f2a4dc3aceee631d7786ae0ba8072b6122ad3b
uchan-nos <uchan0@gmail.com>
2019-08-24 09:51:18 +0900

    window can be dragged. (have a known bug)

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -126,6 +126,27 @@ void LayerManager::Hide(unsigned int id) {
   }
 }
 
+Layer* LayerManager::FindLayerByPosition(Vector2D<int> pos, unsigned int exclude_id) const {
+  auto pred = [pos, exclude_id](Layer* layer) {
+    if (layer->ID() == exclude_id) {
+      return false;
+    }
+    const auto& win = layer->GetWindow();
+    if (!win) {
+      return false;
+    }
+    const auto win_pos = layer->GetPosition();
+    const auto win_end_pos = win_pos + win->Size();
+    return win_pos.x <= pos.x && pos.x < win_end_pos.x &&
+           win_pos.y <= pos.y && pos.y < win_end_pos.y;
+  };
+  auto it = std::find_if(layer_stack_.rbegin(), layer_stack_.rend(), pred);
+  if (it == layer_stack_.rend()) {
+    return nullptr;
+  }
+  return *it;
+}
+
 Layer* LayerManager::FindLayer(unsigned int id) {
   auto pred = [id](const std::unique_ptr<Layer>& elem) {
     return elem->ID() == id;
