165a62aa739f23447bacac5506f575dfe9295a24
uchan-nos <uchan0@gmail.com>
2019-08-25 17:12:34 +0900

    set draggable to layer

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -22,6 +22,15 @@ Vector2D<int> Layer::GetPosition() const {
   return pos_;
 }
 
+Layer& Layer::SetDraggable(bool draggable) {
+  draggable_ = draggable;
+  return *this;
+}
+
+bool Layer::IsDraggable() const {
+  return draggable_;
+}
+
 Layer& Layer::Move(Vector2D<int> pos) {
   pos_ = pos;
   return *this;
--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -31,6 +31,10 @@ class Layer {
   std::shared_ptr<Window> GetWindow() const;
   /** @brief レイヤーの原点座標を取得する。 */
   Vector2D<int> GetPosition() const;
+  /** @brief true でレイヤーがドラッグ移動可能となる。 */
+  Layer& SetDraggable(bool draggable);
+  /** @brief レイヤーがドラッグ移動可能なら true を返す。 */
+  bool IsDraggable() const;
 
   /** @brief レイヤーの位置情報を指定された絶対座標へと更新する。再描画はしない。 */
   Layer& Move(Vector2D<int> pos);
@@ -42,8 +46,9 @@ class Layer {
 
  private:
   unsigned int id_;
-  Vector2D<int> pos_;
-  std::shared_ptr<Window> window_;
+  Vector2D<int> pos_{};
+  std::shared_ptr<Window> window_{};
+  bool draggable_{false};
 };
 
 /** @brief LayerManager は複数のレイヤーを管理する。 */
