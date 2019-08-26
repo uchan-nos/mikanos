108c83df082f8d4e2f2a3686d5b63a8f92b31dfd
uchan-nos <uchan0@gmail.com>
2019-08-26 20:28:51 +0900

    move mouse-related programs to mouse.cpp

--- kernel/mouse.cpp
+++ kernel/mouse.cpp
@@ -1,6 +1,8 @@
 #include "mouse.hpp"
 
 #include "graphics.hpp"
+#include "layer.hpp"
+#include "usb/classdriver/mouse.hpp"
 
 namespace {
   const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
@@ -43,4 +45,60 @@ void DrawMouseCursor(PixelWriter* pixel_writer, Vector2D<int> position) {
       }
     }
   }
+}
+
+Mouse::Mouse(unsigned int layer_id) : layer_id_{layer_id} {
+}
+
+void Mouse::SetPosition(Vector2D<int> position) {
+  position_ = position;
+  layer_manager->Move(layer_id_, position_);
+}
+
+void Mouse::OnInterrupt(uint8_t buttons, int8_t displacement_x, int8_t displacement_y) {
+  const auto oldpos = position_;
+  auto newpos = position_ + Vector2D<int>{displacement_x, displacement_y};
+  newpos = ElementMin(newpos, ScreenSize() + Vector2D<int>{-1, -1});
+  position_ = ElementMax(newpos, {0, 0});
+
+  const auto posdiff = position_ - oldpos;
+
+  layer_manager->Move(layer_id_, position_);
+
+  const bool previous_left_pressed = (previous_buttons_ & 0x01);
+  const bool left_pressed = (buttons & 0x01);
+  if (!previous_left_pressed && left_pressed) {
+    auto layer = layer_manager->FindLayerByPosition(position_, layer_id_);
+    if (layer && layer->IsDraggable()) {
+      drag_layer_id_ = layer->ID();
+    }
+  } else if (previous_left_pressed && left_pressed) {
+    if (drag_layer_id_ > 0) {
+      layer_manager->MoveRelative(drag_layer_id_, posdiff);
+    }
+  } else if (previous_left_pressed && !left_pressed) {
+    drag_layer_id_ = 0;
+  }
+
+  previous_buttons_ = buttons;
+}
+
+std::shared_ptr<Mouse> MakeMouse() {
+  auto mouse_window = std::make_shared<Window>(
+      kMouseCursorWidth, kMouseCursorHeight, screen_config.pixel_format);
+  mouse_window->SetTransparentColor(kMouseTransparentColor);
+  DrawMouseCursor(mouse_window->Writer(), {0, 0});
+
+  auto mouse_layer_id = layer_manager->NewLayer()
+    .SetWindow(mouse_window)
+    .ID();
+
+  auto mouse = std::make_shared<Mouse>(mouse_layer_id);
+  mouse->SetPosition({200, 200});
+  usb::HIDMouseDriver::default_observer =
+    [mouse](uint8_t buttons, int8_t displacement_x, int8_t displacement_y) {
+      mouse->OnInterrupt(buttons, displacement_x, displacement_y);
+    };
+
+  return mouse;
 }
