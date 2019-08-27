f52e33643bf5b013ab37e38843a3735c3e2dc50f
uchan-nos <uchan0@gmail.com>
2019-08-27 17:44:41 +0900

    tick timer manager

--- kernel/timer.cpp
+++ kernel/timer.cpp
@@ -12,9 +12,11 @@ namespace {
 
 // #@@range_begin(init_timer)
 void InitializeLAPICTimer() {
+  timer_manager = new TimerManager;
+
   divide_config = 0b1011; // divide 1:1
   lvt_timer = (0b010 << 16) | InterruptVector::kLAPICTimer; // not-masked, periodic
-  initial_count = kCountMax;
+  initial_count = 0x1000000u;
 }
 // #@@range_end(init_timer)
 
@@ -28,4 +30,14 @@ uint32_t LAPICTimerElapsed() {
 
 void StopLAPICTimer() {
   initial_count = 0;
+}
+
+void TimerManager::Tick() {
+  ++tick_;
+}
+
+TimerManager* timer_manager;
+
+void LAPICTimerOnInterrupt() {
+  timer_manager->Tick();
 }
