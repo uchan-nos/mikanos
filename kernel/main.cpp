55e80992ca2b121ec6ceca2d04fd8afb4e6cbe15
uchan-nos <uchan0@gmail.com>
2019-08-27 10:06:55 +0900

    refactor xhci code

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -62,14 +62,11 @@ extern "C" void KernelMainNewStack(
   InitializeSegmentation();
   InitializePaging();
   InitializeMemoryManager(memory_map);
-
   ::main_queue = new std::deque<Message>(32);
-
-  InitializePCI();
-
   InitializeInterrupt(main_queue);
 
-  auto xhc = usb::xhci::MakeRunController();
+  InitializePCI();
+  usb::xhci::Initialize();
 
   const auto screen_size = ScreenSize();
 
@@ -141,7 +138,7 @@ extern "C" void KernelMainNewStack(
 
     switch (msg.type) {
     case Message::kInterruptXHCI:
-      usb::xhci::ProcessEvents(xhc);
+      usb::xhci::ProcessEvents();
       break;
     default:
       Log(kError, "Unknown message type: %d\n", msg.type);
