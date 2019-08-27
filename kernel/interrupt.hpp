92ddc61e9ca0961d56325676ad6806261cdd085f
uchan-nos <uchan0@gmail.com>
2019-08-27 09:53:01 +0900

    move interrupt code to interrupt.cpp

--- kernel/interrupt.hpp
+++ kernel/interrupt.hpp
@@ -8,8 +8,10 @@
 
 #include <array>
 #include <cstdint>
+#include <deque>
 
 #include "x86_descriptor.hpp"
+#include "message.hpp"
 
 union InterruptDescriptorAttribute {
   uint16_t data;
@@ -68,3 +70,5 @@ struct InterruptFrame {
 };
 
 void NotifyEndOfInterrupt();
+
+void InitializeInterrupt(std::deque<Message>* msg_queue);
