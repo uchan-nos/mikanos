a7922a781e7eeb86cfb6da3d866ce116c281f213
uchan-nos <uchan0@gmail.com>
2019-08-27 17:44:41 +0900

    tick timer manager

--- kernel/timer.hpp
+++ kernel/timer.hpp
@@ -6,3 +6,16 @@ void InitializeLAPICTimer();
 void StartLAPICTimer();
 uint32_t LAPICTimerElapsed();
 void StopLAPICTimer();
+
+class TimerManager {
+ public:
+  void Tick();
+  unsigned long CurrentTick() const { return tick_; }
+
+ private:
+  unsigned long tick_{0};
+};
+
+extern TimerManager* timer_manager;
+
+void LAPICTimerOnInterrupt();
