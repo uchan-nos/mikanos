87b32c3eb8a8eeb872fde1b53bb305e671e3204e
uchan-nos <uchan0@gmail.com>
2019-08-27 14:07:03 +0900

    interrupt by lapic timer

--- kernel/timer.cpp
+++ kernel/timer.cpp
@@ -1,5 +1,7 @@
 #include "timer.hpp"
 
+#include "interrupt.hpp"
+
 namespace {
   const uint32_t kCountMax = 0xffffffffu;
   volatile uint32_t& lvt_timer = *reinterpret_cast<uint32_t*>(0xfee00320);
@@ -10,7 +12,8 @@ namespace {
 
 void InitializeLAPICTimer() {
   divide_config = 0b1011; // divide 1:1
-  lvt_timer = (0b001 << 16) | 32; // masked, one-shot
+  lvt_timer = (0b010 << 16) | InterruptVector::kLAPICTimer; // not-masked, periodic
+  initial_count = kCountMax;
 }
 
 void StartLAPICTimer() {
