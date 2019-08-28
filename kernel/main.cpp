5fd9218830979cb71f983bc573b8ba5277a22952
uchan-nos <uchan0@gmail.com>
2019-08-28 16:57:22 +0900

    timer usage sample

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -92,6 +92,9 @@ extern "C" void KernelMainNewStack(
 
   InitializeLAPICTimer(*main_queue);
 
+  timer_manager->AddTimer(Timer(200, 2));
+  timer_manager->AddTimer(Timer(600, -1));
+
   char str[128];
 
   while (true) {
@@ -119,7 +122,12 @@ extern "C" void KernelMainNewStack(
       usb::xhci::ProcessEvents();
       break;
     case Message::kTimerTimeout:
-      printk("Timer timeout\n");
+      printk("Timer: timeout = %lu, value = %d\n",
+          msg.arg.timer.timeout, msg.arg.timer.value);
+      if (msg.arg.timer.value > 0) {
+        timer_manager->AddTimer(Timer(
+            msg.arg.timer.timeout + 100, msg.arg.timer.value + 1));
+      }
       break;
     default:
       Log(kError, "Unknown message type: %d\n", msg.type);
