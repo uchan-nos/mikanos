7d6fd0639f78bd5ceaee53b412a0eb73bc02a649
uchan-nos <uchan0@gmail.com>
2019-08-28 13:11:51 +0900

    multiple timers and timeout feature

--- kernel/timer.cpp
+++ kernel/timer.cpp
@@ -10,8 +10,8 @@ namespace {
   volatile uint32_t& divide_config = *reinterpret_cast<uint32_t*>(0xfee003e0);
 }
 
-void InitializeLAPICTimer() {
-  timer_manager = new TimerManager;
+void InitializeLAPICTimer(std::deque<Message>& msg_queue) {
+  timer_manager = new TimerManager{msg_queue};
 
   divide_config = 0b1011; // divide 1:1
   lvt_timer = (0b010 << 16) | InterruptVector::kLAPICTimer; // not-masked, periodic
@@ -30,8 +30,34 @@ void StopLAPICTimer() {
   initial_count = 0;
 }
 
+Timer::Timer(unsigned long timeout, int value)
+    : timeout_{timeout}, value_{value} {
+}
+
+TimerManager::TimerManager(std::deque<Message>& msg_queue)
+    : msg_queue_{msg_queue} {
+  timers_.push(Timer{std::numeric_limits<unsigned long>::max(), -1});
+}
+
+void TimerManager::AddTimer(const Timer& timer) {
+  timers_.push(timer);
+}
+
 void TimerManager::Tick() {
   ++tick_;
+  while (true) {
+    const auto& t = timers_.top();
+    if (t.Timeout() > tick_) {
+      break;
+    }
+
+    Message m{Message::kTimerTimeout};
+    m.arg.timer.timeout = t.Timeout();
+    m.arg.timer.value = t.Value();
+    msg_queue_.push_back(m);
+
+    timers_.pop();
+  }
 }
 
 TimerManager* timer_manager;
