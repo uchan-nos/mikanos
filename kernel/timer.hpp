4b1ef84ea5284e0469623a4797bf54c6e67e4014
uchan-nos <uchan0@gmail.com>
2019-08-28 13:11:51 +0900

    multiple timers and timeout feature

--- kernel/timer.hpp
+++ kernel/timer.hpp
@@ -1,19 +1,42 @@
 #pragma once
 
 #include <cstdint>
+#include <queue>
+#include <vector>
+#include "message.hpp"
 
-void InitializeLAPICTimer();
+void InitializeLAPICTimer(std::deque<Message>& msg_queue);
 void StartLAPICTimer();
 uint32_t LAPICTimerElapsed();
 void StopLAPICTimer();
 
+class Timer {
+ public:
+  Timer(unsigned long timeout, int value);
+  unsigned long Timeout() const { return timeout_; }
+  int Value() const { return value_; }
+
+ private:
+  unsigned long timeout_;
+  int value_;
+};
+
+/** @brief タイマー優先度を比較する。タイムアウトが遠いほど優先度低。 */
+inline bool operator<(const Timer& lhs, const Timer& rhs) {
+  return lhs.Timeout() > rhs.Timeout();
+}
+
 class TimerManager {
  public:
+  TimerManager(std::deque<Message>& msg_queue);
+  void AddTimer(const Timer& timer);
   void Tick();
   unsigned long CurrentTick() const { return tick_; }
 
  private:
   volatile unsigned long tick_{0};
+  std::priority_queue<Timer> timers_{};
+  std::deque<Message>& msg_queue_;
 };
 
 extern TimerManager* timer_manager;
