ec3a532f2dd5dc0a14a274e5466743e329c0cc91
uchan-nos <uchan0@gmail.com>
2019-08-29 18:55:38 +0900

    pass RSDP to kernel main function

---
+++ kernel/acpi.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file acpi.hpp
+ *
+ * ACPI テーブル定義や操作用プログラムを集めたファイル。
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace acpi {
+
+struct RSDP {
+  char signature[8];
+  uint8_t checksum;
+  char oem_id[6];
+  uint8_t revision;
+  uint32_t rsdt_address;
+  uint32_t length;
+  uint64_t xsdt_address;
+  uint8_t extended_checksum;
+  char reserved[3];
+} __attribute__((packed));
+
+bool IsValid(const RSDP& rsdp);
+void Initialize(const RSDP& rsdp);
+
+} // namespace acpi
