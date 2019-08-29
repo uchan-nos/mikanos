58f1c9634ff38f2367e34b9ca8055387965d4e22
uchan-nos <uchan0@gmail.com>
2019-08-29 18:55:38 +0900

    pass RSDP to kernel main function

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -347,10 +347,22 @@ EFI_STATUS EFIAPI UefiMain(
       Halt();
   }
 
+  // #@@range_begin(get_acpitable)
+  VOID* acpi_table = NULL;
+  for (UINTN i = 0; i < system_table->NumberOfTableEntries; ++i) {
+    if (CompareGuid(&gEfiAcpiTableGuid,
+                    &system_table->ConfigurationTable[i].VendorGuid)) {
+      acpi_table = system_table->ConfigurationTable[i].VendorTable;
+      break;
+    }
+  }
+
   typedef void EntryPointType(const struct FrameBufferConfig*,
-                              const struct MemoryMap*);
+                              const struct MemoryMap*,
+                              const VOID*);
   EntryPointType* entry_point = (EntryPointType*)entry_addr;
-  entry_point(&config, &memmap);
+  entry_point(&config, &memmap, acpi_table);
+  // #@@range_end(get_acpitable)
 
   Print(L"All done\n");
 
