ef00d34b03edc30ac8358dd870b789d19c6c12fb
uchan-nos <uchan0@gmail.com>
2019-08-30 11:43:58 +0900

    make IsValid a method of RSDP

--- kernel/acpi.hpp
+++ kernel/acpi.hpp
@@ -20,9 +20,10 @@ struct RSDP {
   uint64_t xsdt_address;
   uint8_t extended_checksum;
   char reserved[3];
+
+  bool IsValid() const;
 } __attribute__((packed));
 
-bool IsValid(const RSDP& rsdp);
 void Initialize(const RSDP& rsdp);
 
 } // namespace acpi
