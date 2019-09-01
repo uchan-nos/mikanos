70b2e6127429442b663849362eded835e63fa70e
uchan-nos <uchan0@gmail.com>
2019-09-01 15:43:41 +0900

    measure LAPIC timer freq using ACPI PM timer

--- kernel/acpi.cpp
+++ kernel/acpi.cpp
@@ -2,6 +2,7 @@
 
 #include <cstring>
 #include <cstdlib>
+#include "asmfunc.h"
 #include "logger.hpp"
 
 namespace {
@@ -66,6 +67,22 @@ size_t XSDT::Count() const {
 }
 
 const FADT* fadt;
+
+// #@@range_begin(wait_ms)
+void WaitMilliseconds(unsigned long msec) {
+  const bool pm_timer_32 = (fadt->flags >> 8) & 1;
+  const uint32_t start = IoIn32(fadt->pm_tmr_blk);
+  uint32_t end = start + kPMTimerFreq * msec / 1000;
+  if (!pm_timer_32) {
+    end &= 0x00ffffffu;
+  }
+
+  if (end < start) { // overflow
+    while (IoIn32(fadt->pm_tmr_blk) >= start);
+  }
+  while (IoIn32(fadt->pm_tmr_blk) < end);
+}
+// #@@range_end(wait_ms)
 
 void Initialize(const RSDP& rsdp) {
   if (!rsdp.IsValid()) {
