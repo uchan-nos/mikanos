7125b41282d1e766463964a3135a3db5d4de7f03
uchan-nos <uchan0@gmail.com>
2019-09-03 21:22:38 +0900

    window with a text box

--- kernel/graphics.hpp
+++ kernel/graphics.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <algorithm>
+#include <cstdint>
 #include "frame_buffer_config.hpp"
 
 struct PixelColor {
   uint8_t r, g, b;
 };
+
+constexpr PixelColor ToColor(uint32_t c) {
+  return {
+    static_cast<uint8_t>((c >> 16) & 0xff),
+    static_cast<uint8_t>((c >> 8) & 0xff),
+    static_cast<uint8_t>(c & 0xff)
+  };
+}
 
 inline bool operator==(const PixelColor& lhs, const PixelColor& rhs) {
   return lhs.r == rhs.r && lhs.g == rhs.g && lhs.b == rhs.b;
