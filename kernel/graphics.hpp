d36aecff96b54b435b566b8431982180145303b8
uchan-nos <uchan0@gmail.com>
2019-09-04 08:08:56 +0900

    add review tags

--- kernel/graphics.hpp
+++ kernel/graphics.hpp
@@ -8,6 +8,7 @@ struct PixelColor {
   uint8_t r, g, b;
 };
 
+// #@@range_begin(tocolor)
 constexpr PixelColor ToColor(uint32_t c) {
   return {
     static_cast<uint8_t>((c >> 16) & 0xff),
@@ -15,6 +16,7 @@ constexpr PixelColor ToColor(uint32_t c) {
     static_cast<uint8_t>(c & 0xff)
   };
 }
+// #@@range_end(tocolor)
 
 inline bool operator==(const PixelColor& lhs, const PixelColor& rhs) {
   return lhs.r == rhs.r && lhs.g == rhs.g && lhs.b == rhs.b;
