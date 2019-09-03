7da6ebf19816e2be3c719e48a6dc2a323bd78da0
uchan-nos <uchan0@gmail.com>
2019-09-03 13:28:00 +0900

    remove review tags

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -96,9 +96,7 @@ extern "C" void KernelMainNewStack(
   acpi::Initialize(acpi_table);
   InitializeLAPICTimer(*main_queue);
 
-  // #@@range_begin(call_initkb)
   InitializeKeyboard(*main_queue);
-  // #@@range_end(call_initkb)
 
   char str[128];
 
@@ -128,13 +126,11 @@ extern "C" void KernelMainNewStack(
       break;
     case Message::kTimerTimeout:
       break;
-    // #@@range_begin(event_handling)
     case Message::kKeyPush:
       if (msg.arg.keyboard.ascii != 0) {
         printk("%c", msg.arg.keyboard.ascii);
       }
       break;
-    // #@@range_end(event_handling)
     default:
       Log(kError, "Unknown message type: %d\n", msg.type);
     }
