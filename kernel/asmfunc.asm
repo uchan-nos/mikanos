2b89cbc130e6c3f9e4e921a5cec5fb7e7a1c279c
uchan-nos <uchan0@gmail.com>
2019-09-06 17:03:45 +0900

    switch 2 tasks (cooperative multitask)

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -86,3 +86,43 @@ KernelMain:
 .fin:
     hlt
     jmp .fin
+
+#@range_begin(switch_context)
+global SwitchContext
+SwitchContext:  ; void SwitchContext(CPURegisters* to, CPURegisters* current);
+    mov [rsi +  2*8], rax
+    mov [rsi +  3*8], rbx
+    mov [rsi +  4*8], rcx
+    mov [rsi +  5*8], rdx
+    mov [rsi +  6*8], rdi
+    mov [rsi +  7*8], rsi
+    mov [rsi +  8*8], rbp
+    mov [rsi +  9*8], rsp
+    mov [rsi + 10*8], r8
+    mov [rsi + 11*8], r9
+    mov [rsi + 12*8], r10
+    mov [rsi + 13*8], r11
+    mov [rsi + 14*8], r12
+    mov [rsi + 15*8], r13
+    mov [rsi + 16*8], r14
+    mov [rsi + 17*8], r15
+
+    mov rax, [rdi +  2*8]
+    mov rbx, [rdi +  3*8]
+    mov rcx, [rdi +  4*8]
+    mov rdx, [rdi +  5*8]
+    mov rsi, [rdi +  7*8]
+    mov rbp, [rdi +  8*8]
+    mov rsp, [rdi +  9*8]
+    mov r8,  [rdi + 10*8]
+    mov r9,  [rdi + 11*8]
+    mov r10, [rdi + 12*8]
+    mov r11, [rdi + 13*8]
+    mov r12, [rdi + 14*8]
+    mov r13, [rdi + 15*8]
+    mov r14, [rdi + 16*8]
+    mov r15, [rdi + 17*8]
+
+    mov rdi, [rdi +  6*8]
+    ret
+#@range_end(switch_context)
