5df97e77773dd59794b9a9d33de11af8b55179e2
uchan-nos <uchan0@gmail.com>
2019-09-07 12:59:02 +0900

    use rsp variable instead of whole CPURegisters

--- kernel/task.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-struct CPURegisters {
-  uint64_t rip, rflags;
-  uint64_t rax, rbx, rcx, rdx, rdi, rsi, rbp, rsp;
-  uint64_t r8, r9, r10, r11, r12, r13, r14, r15;
-} __attribute__((packed));
