66c7b1cb97a4e68a12cc222265be9922696f0a9d
uchan-nos <uchan0@gmail.com>
2019-09-10 15:29:23 +0900

    handle any number of tasks

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -131,8 +131,8 @@ void InitializeTaskBWindow() {
   layer_manager->UpDown(task_b_window_layer_id, std::numeric_limits<int>::max());
 }
 
-void TaskB(int task_id, int data) {
-  printk("TaskB: task_id=%d, data=%d\n", task_id, data);
+void TaskB(uint64_t task_id, int64_t data) {
+  printk("TaskB: task_id=%lu, data=%lu\n", task_id, data);
   char str[128];
   int count = 0;
   while (true) {
@@ -143,6 +143,13 @@ void TaskB(int task_id, int data) {
     layer_manager->Draw(task_b_window_layer_id);
   }
 }
+
+// #@@range_begin(taskc)
+void TaskC(uint64_t task_id, int64_t data) {
+  printk("TaskC: task_id=%lu, data=%lx\n", task_id, data);
+  while (true) __asm__("hlt");
+}
+// #@@range_end(taskc)
 
 std::deque<Message>* main_queue;
 
@@ -188,24 +195,12 @@ extern "C" void KernelMainNewStack(
   __asm__("sti");
   bool textbox_cursor_visible = false;
 
-  std::vector<uint64_t> task_b_stack(1024);
-  uint64_t task_b_stack_end = reinterpret_cast<uint64_t>(&task_b_stack[1024]);
-
-  memset(&task_b_ctx, 0, sizeof(task_b_ctx));
-  task_b_ctx.rip = reinterpret_cast<uint64_t>(TaskB);
-  task_b_ctx.rdi = 1;
-  task_b_ctx.rsi = 43;
-
-  task_b_ctx.cr3 = GetCR3();
-  task_b_ctx.rflags = 0x202;
-  task_b_ctx.cs = kKernelCS;
-  task_b_ctx.ss = kKernelSS;
-  task_b_ctx.rsp = (task_b_stack_end & ~0xflu) - 8;
-
-  // MXCSR のすべての例外をマスクする
-  *reinterpret_cast<uint32_t*>(&task_b_ctx.fxsave_area[24]) = 0x1f80;
-
+  // #@@range_begin(call_inittask)
   InitializeTask();
+  task_manager->NewTask().InitContext(TaskB, 45);
+  task_manager->NewTask().InitContext(TaskC, 0xdeadbeef);
+  task_manager->NewTask().InitContext(TaskC, 0xcafebabe);
+  // #@@range_end(call_inittask)
 
   char str[128];
 
