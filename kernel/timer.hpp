3744ab71a130aa4eaf86e8bbbf532b032e0367f7
uchan-nos <uchan0@gmail.com>
2019-09-10 09:21:36 +0900

    task timer 1 second

--- kernel/timer.hpp
+++ kernel/timer.hpp
@@ -45,7 +45,7 @@ extern unsigned long lapic_timer_freq;
 const int kTimerFreq = 100;
 
 // #@@range_begin(tasktimer)
-const int kTaskTimerPeriod = static_cast<int>(kTimerFreq * 0.02);
+const int kTaskTimerPeriod = static_cast<int>(kTimerFreq * 1.0);
 const int kTaskTimerValue = std::numeric_limits<int>::min();
 // #@@range_end(tasktimer)
 
