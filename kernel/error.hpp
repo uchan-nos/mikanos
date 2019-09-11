8ff90263a491fcb207e8fb5f904eed7143ab37a8
uchan-nos <uchan0@gmail.com>
2019-09-11 23:27:08 +0900

    task sleep & wakeup

--- kernel/error.hpp
+++ kernel/error.hpp
@@ -28,6 +28,7 @@ class Error {
     kNoWaiter,
     kNoPCIMSI,
     kUnknownPixelFormat,
+    kNoSuchTask,
     kLastOfCode,  // この列挙子は常に最後に配置する
   };
 
@@ -55,6 +56,7 @@ class Error {
     "kNoWaiter",
     "kNoPCIMSI",
     "kUnknownPixelFormat",
+    "kNoSuchTask",
   };
   static_assert(Error::Code::kLastOfCode == code_names_.size());
 
