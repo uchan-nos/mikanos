fb32b7a7d935c6d070b6250dc0b5d84daf0e7863
uchan-nos <uchan0@gmail.com>
2019-09-13 09:59:22 +0900

    remove review tags

--- kernel/task.cpp
+++ kernel/task.cpp
@@ -33,7 +33,6 @@ TaskContext& Task::Context() {
   return context_;
 }
 
-// #@@range_begin(task_methods)
 uint64_t Task::ID() const {
   return id_;
 }
@@ -47,20 +46,16 @@ Task& Task::Wakeup() {
   task_manager->Wakeup(this);
   return *this;
 }
-// #@@range_end(task_methods)
 
-// #@@range_begin(taskmgr_ctor)
 TaskManager::TaskManager() {
   running_.push_back(&NewTask());
 }
-// #@@range_end(taskmgr_ctor)
 
 Task& TaskManager::NewTask() {
   ++latest_id_;
   return *tasks_.emplace_back(new Task{latest_id_});
 }
 
-// #@@range_begin(taskmgr_swtask)
 void TaskManager::SwitchTask(bool current_sleep) {
   Task* current_task = running_.front();
   running_.pop_front();
@@ -71,9 +66,7 @@ void TaskManager::SwitchTask(bool current_sleep) {
 
   SwitchContext(&next_task->Context(), &current_task->Context());
 }
-// #@@range_end(taskmgr_swtask)
 
-// #@@range_begin(taskmgr_sleep)
 void TaskManager::Sleep(Task* task) {
   auto it = std::find(running_.begin(), running_.end(), task);
 
@@ -88,9 +81,7 @@ void TaskManager::Sleep(Task* task) {
 
   running_.erase(it);
 }
-// #@@range_end(taskmgr_sleep)
 
-// #@@range_begin(taskmgr_sleep_id)
 Error TaskManager::Sleep(uint64_t id) {
   auto it = std::find_if(tasks_.begin(), tasks_.end(),
                          [id](const auto& t){ return t->ID() == id; });
@@ -101,18 +92,14 @@ Error TaskManager::Sleep(uint64_t id) {
   Sleep(it->get());
   return MAKE_ERROR(Error::kSuccess);
 }
-// #@@range_end(taskmgr_sleep_id)
 
-// #@@range_begin(taskmgr_wakeup)
 void TaskManager::Wakeup(Task* task) {
   auto it = std::find(running_.begin(), running_.end(), task);
   if (it == running_.end()) {
     running_.push_back(task);
   }
 }
-// #@@range_end(taskmgr_wakeup)
 
-// #@@range_begin(taskmgr_wakeup_id)
 Error TaskManager::Wakeup(uint64_t id) {
   auto it = std::find_if(tasks_.begin(), tasks_.end(),
                          [id](const auto& t){ return t->ID() == id; });
@@ -123,7 +110,6 @@ Error TaskManager::Wakeup(uint64_t id) {
   Wakeup(it->get());
   return MAKE_ERROR(Error::kSuccess);
 }
-// #@@range_end(taskmgr_wakeup_id)
 
 TaskManager* task_manager;
 
