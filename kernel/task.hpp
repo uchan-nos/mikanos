c75bdf4ecb7e6025156d2a68c4d16c1e47cbf814
uchan-nos <uchan0@gmail.com>
2019-09-14 17:41:00 +0900

    implement task leveled scheduling

--- kernel/task.hpp
+++ kernel/task.hpp
@@ -26,8 +26,11 @@ struct TaskContext {
 
 using TaskFunc = void (uint64_t, int64_t);
 
+class TaskManager;
+
 class Task {
  public:
+  static const int kDefaultLevel = 1;
   static const size_t kDefaultStackBytes = 4096;
 
   Task(uint64_t id);
@@ -44,25 +47,39 @@ class Task {
   std::vector<uint64_t> stack_;
   alignas(16) TaskContext context_;
   std::deque<Message> msgs_;
+  unsigned int level_{kDefaultLevel};
+  bool running_{false};
+
+  Task& SetLevel(int level) { level_ = level; return *this; }
+  int Level() const { return level_; }
+  Task& SetRunning(bool running) { running_ = running; return *this; }
+  bool Running() const { return running_; }
+
+  friend TaskManager;
 };
 
 class TaskManager {
  public:
+  // level: 0 = lowest, kMaxLevel = highest
+  static const int kMaxLevel = 3;
+
   TaskManager();
   Task& NewTask();
   void SwitchTask(bool current_sleep = false);
 
   void Sleep(Task* task);
   Error Sleep(uint64_t id);
-  void Wakeup(Task* task);
-  Error Wakeup(uint64_t id);
+  void Wakeup(Task* task, int level = -1);
+  Error Wakeup(uint64_t id, int level = -1);
   Error SendMessage(uint64_t id, const Message& msg);
   Task& CurrentTask();
 
  private:
   std::vector<std::unique_ptr<Task>> tasks_{};
   uint64_t latest_id_{0};
-  std::deque<Task*> running_{};
+  std::array<std::deque<Task*>, kMaxLevel + 1> running_{};
+  int current_level_{kMaxLevel};
+  bool level_changed_{false};
 };
 
 extern TaskManager* task_manager;
