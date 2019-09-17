55506d331c81e241f9b94e0cbacd8143c9c3fa7f
uchan-nos <uchan0@gmail.com>
2019-09-17 14:23:00 +0900

    register idle task at lowest level

--- kernel/task.cpp
+++ kernel/task.cpp
@@ -3,6 +3,14 @@
 #include "asmfunc.h"
 #include "timer.hpp"
 
+// #@@range_begin(task_idle)
+namespace {
+  void TaskIdle(uint64_t task_id, int64_t data) {
+    while (true) __asm__("hlt");
+  }
+}
+// #@@range_end(task_idle)
+
 Task::Task(uint64_t id) : id_{id}, msgs_{} {
   const size_t stack_size = kDefaultStackBytes / sizeof(stack_[0]);
   stack_.resize(stack_size);
@@ -82,6 +90,12 @@ TaskManager::TaskManager() {
     .SetLevel(current_level_)
     .SetRunning(true);
   running_[current_level_].push_back(&task);
+
+  Task& idle = NewTask()
+    .PushInitialStack(TaskIdle, 0)
+    .SetLevel(0)
+    .SetRunning(true);
+  running_[0].push_back(&idle);
 }
 // #@@range_end(taskmgr_ctor)
 
@@ -90,7 +104,6 @@ Task& TaskManager::NewTask() {
   return *tasks_.emplace_back(new Task{latest_id_});
 }
 
-// #@@range_begin(switchtask)
 void TaskManager::SwitchTask(bool current_sleep) {
   auto& level_queue = running_[current_level_];
   Task* current_task = level_queue.front();
@@ -116,9 +129,7 @@ void TaskManager::SwitchTask(bool current_sleep) {
 
   SwitchContext(&next_task->StackPointer(), &current_task->StackPointer());
 }
-// #@@range_end(switchtask)
 
-// #@@range_begin(sleep)
 void TaskManager::Sleep(Task* task) {
   if (!task->Running()) {
     return;
@@ -133,7 +144,6 @@ void TaskManager::Sleep(Task* task) {
 
   std::erase(running_[task->Level()], task);
 }
-// #@@range_end(sleep)
 
 Error TaskManager::Sleep(uint64_t id) {
   auto it = std::find_if(tasks_.begin(), tasks_.end(),
@@ -146,7 +156,6 @@ Error TaskManager::Sleep(uint64_t id) {
   return MAKE_ERROR(Error::kSuccess);
 }
 
-// #@@range_begin(wakeup)
 void TaskManager::Wakeup(Task* task, int level) {
   if (task->Running()) {
     ChangeLevelRunning(task, level);
@@ -166,7 +175,6 @@ void TaskManager::Wakeup(Task* task, int level) {
   }
   return;
 }
-// #@@range_end(wakeup)
 
 Error TaskManager::Wakeup(uint64_t id, int level) {
   auto it = std::find_if(tasks_.begin(), tasks_.end(),
@@ -194,7 +202,6 @@ Task& TaskManager::CurrentTask() {
   return *running_[current_level_].front();
 }
 
-// #@@range_begin(chlv_running)
 void TaskManager::ChangeLevelRunning(Task* task, int level) {
   if (level < 0 || level == task->Level()) {
     return;
@@ -222,7 +229,6 @@ void TaskManager::ChangeLevelRunning(Task* task, int level) {
     level_changed_ = true;
   }
 }
-// #@@range_end(chlv_running)
 
 TaskManager* task_manager;
 
