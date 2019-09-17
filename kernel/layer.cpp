d5d37d8bdd0a8700d95d6ead63df891ecd6a7845
uchan-nos <uchan0@gmail.com>
2019-09-17 17:20:27 +0900

    handle layer operations only in main context

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -208,3 +208,20 @@ void InitializeLayer() {
   layer_manager->UpDown(bglayer_id, 0);
   layer_manager->UpDown(console->LayerID(), 1);
 }
+
+// #@@range_begin(proc_layermsg)
+void ProcessLayerMessage(const Message& msg) {
+  const auto& arg = msg.arg.layer;
+  switch (arg.op) {
+  case LayerOperation::Move:
+    layer_manager->Move(arg.layer_id, {arg.x, arg.y});
+    break;
+  case LayerOperation::MoveRelative:
+    layer_manager->MoveRelative(arg.layer_id, {arg.x, arg.y});
+    break;
+  case LayerOperation::Draw:
+    layer_manager->Draw(arg.layer_id);
+    break;
+  }
+}
+// #@@range_end(proc_layermsg)
