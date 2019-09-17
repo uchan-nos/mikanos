420544ee0f35cb4ca6c6eccb453a69bd798096be
uchan-nos <uchan0@gmail.com>
2019-09-17 17:20:27 +0900

    handle layer operations only in main context

--- kernel/message.hpp
+++ kernel/message.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
+// #@@range_begin(layer_op)
+enum class LayerOperation {
+  Move, MoveRelative, Draw
+};
+// #@@range_end(layer_op)
+
+// #@@range_begin(msg_head)
 struct Message {
   enum Type {
     kInterruptXHCI,
     kTimerTimeout,
     kKeyPush,
+    kLayer,
+    kLayerFinish,
   } type;
+
+  uint64_t src_task;
+// #@@range_end(msg_head)
 
   union {
     struct {
@@ -18,5 +30,13 @@ struct Message {
       uint8_t keycode;
       char ascii;
     } keyboard;
+
+    // #@@range_begin(msg_layer)
+    struct {
+      LayerOperation op;
+      unsigned int layer_id;
+      int x, y;
+    } layer;
+    // #@@range_end(msg_layer)
   } arg;
 };
