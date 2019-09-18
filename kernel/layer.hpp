27f0d070b70db8d15bb4a854e4ae9c80a32c17da
uchan-nos <uchan0@gmail.com>
2019-09-18 22:23:01 +0900

    implement toplevel window with activate function

--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -85,6 +85,10 @@ class LayerManager {
 
   /** @brief 指定された座標にウィンドウを持つ最も上に表示されているレイヤーを探す。 */
   Layer* FindLayerByPosition(Vector2D<int> pos, unsigned int exclude_id) const;
+  /** @brief 指定された ID を持つレイヤーを返す。 */
+  Layer* FindLayer(unsigned int id);
+  /** @brief 指定されたレイヤーの現在の高さを返す。 */
+  int GetHeight(unsigned int id);
 
  private:
   FrameBuffer* screen_{nullptr};
@@ -92,11 +96,24 @@ class LayerManager {
   std::vector<std::unique_ptr<Layer>> layers_{};
   std::vector<Layer*> layer_stack_{};
   unsigned int latest_id_{0};
-
-  Layer* FindLayer(unsigned int id);
 };
 
 extern LayerManager* layer_manager;
+
+class ActiveLayer {
+ public:
+  ActiveLayer(LayerManager& manager);
+  void SetMouseLayer(unsigned int mouse_layer);
+  void Activate(unsigned int layer_id);
+  unsigned int GetActive() const { return active_layer_; }
+
+ private:
+  LayerManager& manager_;
+  unsigned int active_layer_{0};
+  unsigned int mouse_layer_{0};
+};
+
+extern ActiveLayer* active_layer;
 
 void InitializeLayer();
 void ProcessLayerMessage(const Message& msg);
