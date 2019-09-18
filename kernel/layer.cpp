2661b920d8363889032fc8fa3ec080c9064fe0f7
uchan-nos <uchan0@gmail.com>
2019-09-18 22:23:01 +0900

    implement toplevel window with activate function

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -169,11 +169,47 @@ Layer* LayerManager::FindLayer(unsigned int id) {
   return it->get();
 }
 
+int LayerManager::GetHeight(unsigned int id) {
+  for (int h = 0; h < layer_stack_.size(); ++h) {
+    if (layer_stack_[h]->ID() == id) {
+      return h;
+    }
+  }
+  return -1;
+}
+
 namespace {
   FrameBuffer* screen;
 }
 
 LayerManager* layer_manager;
+
+ActiveLayer::ActiveLayer(LayerManager& manager) : manager_{manager} {
+}
+
+void ActiveLayer::SetMouseLayer(unsigned int mouse_layer) {
+  mouse_layer_ = mouse_layer;
+}
+
+void ActiveLayer::Activate(unsigned int layer_id) {
+  if (active_layer_ == layer_id) {
+    return;
+  }
+
+  if (active_layer_ > 0) {
+    Layer* layer = manager_.FindLayer(active_layer_);
+    layer->GetWindow()->Deactivate();
+    manager_.Draw(active_layer_);
+  }
+
+  active_layer_ = layer_id;
+  Layer* layer = manager_.FindLayer(active_layer_);
+  layer->GetWindow()->Activate();
+  manager_.UpDown(active_layer_, manager_.GetHeight(mouse_layer_) - 1);
+  manager_.Draw(active_layer_);
+}
+
+ActiveLayer* active_layer;
 
 void InitializeLayer() {
   const auto screen_size = ScreenSize();
@@ -207,6 +243,8 @@ void InitializeLayer() {
 
   layer_manager->UpDown(bglayer_id, 0);
   layer_manager->UpDown(console->LayerID(), 1);
+
+  active_layer = new ActiveLayer{*layer_manager};
 }
 
 void ProcessLayerMessage(const Message& msg) {
