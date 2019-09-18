57c56e1c99a7cd127218fe7ee77ccdb458154992
uchan-nos <uchan0@gmail.com>
2019-09-18 22:23:01 +0900

    implement toplevel window with activate function

--- kernel/mouse.cpp
+++ kernel/mouse.cpp
@@ -73,6 +73,7 @@ void Mouse::OnInterrupt(uint8_t buttons, int8_t displacement_x, int8_t displacem
     auto layer = layer_manager->FindLayerByPosition(position_, layer_id_);
     if (layer && layer->IsDraggable()) {
       drag_layer_id_ = layer->ID();
+      active_layer->Activate(layer->ID());
     }
   } else if (previous_left_pressed && left_pressed) {
     if (drag_layer_id_ > 0) {
@@ -103,4 +104,6 @@ void InitializeMouse() {
     [mouse](uint8_t buttons, int8_t displacement_x, int8_t displacement_y) {
       mouse->OnInterrupt(buttons, displacement_x, displacement_y);
     };
+
+  active_layer->SetMouseLayer(mouse_layer_id);
 }
