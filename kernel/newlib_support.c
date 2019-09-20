b9753600f7c7b694bd8b735138e271cb2e5828f3
uchan-nos <uchan0@gmail.com>
2019-09-20 10:24:05 +0900

    use std::string instead for window title_ variable

--- kernel/newlib_support.c
+++ kernel/newlib_support.c
@@ -26,3 +26,33 @@ int kill(int pid, int sig) {
   errno = EINVAL;
   return -1;
 }
+
+int close(int fd) {
+  errno = EBADF;
+  return -1;
+}
+
+off_t lseek(int fd, off_t offset, int whence) {
+  errno = EBADF;
+  return -1;
+}
+
+ssize_t read(int fd, void* buf, size_t count) {
+  errno = EBADF;
+  return -1;
+}
+
+ssize_t write(int fd, const void* buf, size_t count) {
+  errno = EBADF;
+  return -1;
+}
+
+int fstat(int fd, struct stat* buf) {
+  errno = EBADF;
+  return -1;
+}
+
+int isatty(int fd) {
+  errno = EBADF;
+  return -1;
+}
