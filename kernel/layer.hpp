de22ac901d20155820e829f4d69e5cae04cb5d97
uchan-nos <uchan0@gmail.com>
2019-09-23 22:03:47 +0900

    refactor message creation

--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -119,3 +119,16 @@ extern ActiveLayer* active_layer;
 
 void InitializeLayer();
 void ProcessLayerMessage(const Message& msg);
+
+constexpr Message MakeLayerMessage(
+    uint64_t task_id, unsigned int layer_id,
+    LayerOperation op, const Rectangle<int>& area) {
+  Message msg{Message::kLayer, task_id};
+  msg.arg.layer.layer_id = layer_id;
+  msg.arg.layer.op = op;
+  msg.arg.layer.x = area.pos.x;
+  msg.arg.layer.y = area.pos.y;
+  msg.arg.layer.w = area.size.x;
+  msg.arg.layer.h = area.size.y;
+  return msg;
+}
