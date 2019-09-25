309a5d4a2ff5eee12371d266d4c8b2de17b65020
uchan-nos <uchan0@gmail.com>
2019-09-25 21:28:43 +0900

    remove review tags

--- kernel/terminal.hpp
+++ kernel/terminal.hpp
@@ -15,16 +15,13 @@
 class Terminal {
  public:
   static const int kRows = 15, kColumns = 60;
-  // #@@range_begin(linemax)
   static const int kLineMax = 128;
-  // #@@range_end(linemax)
 
   Terminal();
   unsigned int LayerID() const { return layer_id_; }
   Rectangle<int> BlinkCursor();
   Rectangle<int> InputKey(uint8_t modifier, uint8_t keycode, char ascii);
 
-  // #@@range_begin(term_fields)
  private:
   std::shared_ptr<ToplevelWindow> window_;
   unsigned int layer_id_;
@@ -37,7 +34,6 @@ class Terminal {
   int linebuf_index_{0};
   std::array<char, kLineMax> linebuf_{};
   void Scroll1();
-  // #@@range_end(term_fields)
 };
 
 void TaskTerminal(uint64_t task_id, int64_t data);
