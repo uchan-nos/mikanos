1dfc4f338409475c593578b2bf22d7fcf4917442
uchan-nos <uchan0@gmail.com>
2019-10-01 10:12:12 +0900

    implement command history

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -19,7 +19,10 @@ Terminal::Terminal() {
     .SetDraggable(true)
     .ID();
 
+  // #@@range_begin(resize_history)
   Print(">");
+  cmd_history_.resize(8);
+  // #@@range_end(resize_history)
 }
 
 Rectangle<int> Terminal::BlinkCursor() {
@@ -45,9 +48,17 @@ Rectangle<int> Terminal::InputKey(
 
   Rectangle<int> draw_area{CalcCursorPos(), {8*2, 16}};
 
+  // #@@range_begin(handle_enter)
   if (ascii == '\n') {
     linebuf_[linebuf_index_] = 0;
     linebuf_index_ = 0;
+    if (linebuf_index_ > 0) {
+      cmd_history_.pop_back();
+      cmd_history_.push_front(linebuf_);
+    }
+    cmd_history_index_ = -1;
+  // #@@range_end(handle_enter)
+
     cursor_.x = 0;
     if (cursor_.y < kRows - 1) {
       ++cursor_.y;
@@ -75,7 +86,13 @@ Rectangle<int> Terminal::InputKey(
       WriteAscii(*window_->Writer(), CalcCursorPos(), ascii, {255, 255, 255});
       ++cursor_.x;
     }
+  // #@@range_begin(handle_arrow)
+  } else if (keycode == 0x51) { // down arrow
+    draw_area = HistoryUpDown(-1);
+  } else if (keycode == 0x52) { // up arrow
+    draw_area = HistoryUpDown(1);
   }
+  // #@@range_end(handle_arrow)
 
   DrawCursor(true);
 
@@ -155,6 +172,34 @@ void Terminal::Print(const char* s) {
 
   DrawCursor(true);
 }
+
+// #@@range_begin(history_updown)
+Rectangle<int> Terminal::HistoryUpDown(int direction) {
+  if (direction == -1 && cmd_history_index_ >= 0) {
+    --cmd_history_index_;
+  } else if (direction == 1 && cmd_history_index_ + 1 < cmd_history_.size()) {
+    ++cmd_history_index_;
+  }
+
+  cursor_.x = 1;
+  const auto first_pos = CalcCursorPos();
+
+  Rectangle<int> draw_area{first_pos, {8*(kColumns - 1), 16}};
+  FillRectangle(*window_->Writer(), draw_area.pos, draw_area.size, {0, 0, 0});
+
+  const char* history = "";
+  if (cmd_history_index_ >= 0) {
+    history = &cmd_history_[cmd_history_index_][0];
+  }
+
+  strcpy(&linebuf_[0], history);
+  linebuf_index_ = strlen(history);
+
+  WriteString(*window_->Writer(), first_pos, history, {255, 255, 255});
+  cursor_.x = linebuf_index_ + 1;
+  return draw_area;
+}
+// #@@range_end(history_updown)
 
 void TaskTerminal(uint64_t task_id, int64_t data) {
   __asm__("cli");
