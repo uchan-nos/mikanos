ff6484e5d13ba324809b32392e052b939e254f8d
uchan-nos <uchan0@gmail.com>
2019-10-02 10:11:55 +0900

    remove review tags

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -19,10 +19,8 @@ Terminal::Terminal() {
     .SetDraggable(true)
     .ID();
 
-  // #@@range_begin(resize_history)
   Print(">");
   cmd_history_.resize(8);
-  // #@@range_end(resize_history)
 }
 
 Rectangle<int> Terminal::BlinkCursor() {
@@ -48,7 +46,6 @@ Rectangle<int> Terminal::InputKey(
 
   Rectangle<int> draw_area{CalcCursorPos(), {8*2, 16}};
 
-  // #@@range_begin(handle_enter)
   if (ascii == '\n') {
     linebuf_[linebuf_index_] = 0;
     if (linebuf_index_ > 0) {
@@ -57,7 +54,6 @@ Rectangle<int> Terminal::InputKey(
     }
     linebuf_index_ = 0;
     cmd_history_index_ = -1;
-  // #@@range_end(handle_enter)
 
     cursor_.x = 0;
     if (cursor_.y < kRows - 1) {
@@ -86,13 +82,11 @@ Rectangle<int> Terminal::InputKey(
       WriteAscii(*window_->Writer(), CalcCursorPos(), ascii, {255, 255, 255});
       ++cursor_.x;
     }
-  // #@@range_begin(handle_arrow)
   } else if (keycode == 0x51) { // down arrow
     draw_area = HistoryUpDown(-1);
   } else if (keycode == 0x52) { // up arrow
     draw_area = HistoryUpDown(1);
   }
-  // #@@range_end(handle_arrow)
 
   DrawCursor(true);
 
@@ -173,7 +167,6 @@ void Terminal::Print(const char* s) {
   DrawCursor(true);
 }
 
-// #@@range_begin(history_updown)
 Rectangle<int> Terminal::HistoryUpDown(int direction) {
   if (direction == -1 && cmd_history_index_ >= 0) {
     --cmd_history_index_;
@@ -199,7 +192,6 @@ Rectangle<int> Terminal::HistoryUpDown(int direction) {
   cursor_.x = linebuf_index_ + 1;
   return draw_area;
 }
-// #@@range_end(history_updown)
 
 void TaskTerminal(uint64_t task_id, int64_t data) {
   __asm__("cli");
