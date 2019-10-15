3a28bff16e82386b2a15fc52826050b86de1234e
uchan-nos <uchan0@gmail.com>
2019-10-15 11:30:56 +0900

    remove review tags

--- kernel/fat.cpp
+++ kernel/fat.cpp
@@ -4,15 +4,12 @@
 
 namespace fat {
 
-// #@@range_begin(init_fat)
 BPB* boot_volume_image;
 
 void Initialize(void* volume_image) {
   boot_volume_image = reinterpret_cast<fat::BPB*>(volume_image);
 }
-// #@@range_end(init_fat)
 
-// #@@range_begin(get_cluster_addr)
 uintptr_t GetClusterAddr(unsigned long cluster) {
   unsigned long sector_num =
     boot_volume_image->reserved_sector_count +
@@ -21,9 +18,7 @@ uintptr_t GetClusterAddr(unsigned long cluster) {
   uintptr_t offset = sector_num * boot_volume_image->bytes_per_sector;
   return reinterpret_cast<uintptr_t>(boot_volume_image) + offset;
 }
-// #@@range_end(get_cluster_addr)
 
-// #@@range_begin(read_name)
 void ReadName(const DirectoryEntry& entry, char* base, char* ext) {
   memcpy(base, &entry.name[0], 8);
   base[8] = 0;
@@ -37,6 +32,5 @@ void ReadName(const DirectoryEntry& entry, char* base, char* ext) {
     ext[i] = 0;
   }
 }
-// #@@range_end(read_name)
 
 } // namespace fat
