08be8b0a0f331f9e1cd6265ea9302d0b0e4f878f
uchan-nos <uchan0@gmail.com>
2019-10-18 07:56:52 +0900

    add review tag

--- kernel/fat.hpp
+++ kernel/fat.hpp
@@ -50,6 +50,7 @@ enum class Attribute : uint8_t {
   kLongName  = 0x0f,
 };
 
+// #@@range_begin(directory_entry)
 struct DirectoryEntry {
   unsigned char name[11];
   Attribute attr;
@@ -69,6 +70,7 @@ struct DirectoryEntry {
       (static_cast<uint32_t>(first_cluster_high) << 16);
   }
 } __attribute__((packed));
+// #@@range_end(directory_entry)
 
 extern BPB* boot_volume_image;
 extern unsigned long bytes_per_cluster;
