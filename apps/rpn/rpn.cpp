58169e10f4cbc5df3725666ef152e3e4522e3a33
uchan-nos <uchan0@gmail.com>
2019-12-01 10:30:17 +0900

    rpn command draws using kernel function

--- apps/rpn/rpn.cpp
+++ apps/rpn/rpn.cpp
@@ -1,5 +1,12 @@
+// #@@range_begin(kernelfunc_decl)
 #include <cstring>
 #include <cstdlib>
+#include "../../kernel/graphics.hpp"
+
+auto& printk = *reinterpret_cast<int (*)(const char*, ...)>(0x000000000010b000);
+auto& fill_rect = *reinterpret_cast<decltype(FillRectangle)*>(0x000000000010c1c0);
+auto& scrn_writer = *reinterpret_cast<decltype(screen_writer)*>(0x000000000024d078);
+// #@@range_end(kernelfunc_decl)
 
 int stack_ptr;
 long stack[100];
@@ -15,6 +22,7 @@ void Push(long value) {
   stack[stack_ptr] = value;
 }
 
+// #@@range_begin(kernelfunc_call)
 extern "C" int main(int argc, char** argv) {
   stack_ptr = -1;
 
@@ -23,17 +31,24 @@ extern "C" int main(int argc, char** argv) {
       long b = Pop();
       long a = Pop();
       Push(a + b);
+      printk("[%d] <- %ld\n", stack_ptr, a + b);
     } else if (strcmp(argv[i], "-") == 0) {
       long b = Pop();
       long a = Pop();
       Push(a - b);
+      printk("[%d] <- %ld\n", stack_ptr, a - b);
     } else {
       long a = atol(argv[i]);
       Push(a);
+      printk("[%d] <- %ld\n", stack_ptr, a);
     }
   }
+
+  fill_rect(*scrn_writer, Vector2D<int>{100, 10}, Vector2D<int>{200, 200}, ToColor(0x00ff00));
+
   if (stack_ptr < 0) {
     return 0;
   }
   return static_cast<int>(Pop());
 }
+// #@@range_end(kernelfunc_call)
