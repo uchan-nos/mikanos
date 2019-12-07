8df8ee7961a0f4ea4bbae7b99a5a58a621234492
uchan-nos <uchan0@gmail.com>
2019-12-07 15:48:51 +0900

    change syscall names

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -2,22 +2,29 @@
 
 #include "logger.hpp"
 
+namespace syscall {
+
+#define SYSCALL(name) \
+  uint64_t name( \
+      uint64_t arg1, uint64_t arg2, uint64_t arg3, \
+      uint64_t arg4, uint64_t arg5, uint64_t arg6)
+
+SYSCALL(PutChar) {
+  Log(kWarn, "%c", arg1 & 0xff);
+  return 0;
+}
+
+SYSCALL(PutString) {
+  Log(kWarn, "%s", reinterpret_cast<char*>(arg1));
+  return 0;
+}
+
+#undef SYSCALL
+
+} // namespace syscall
+
 using SyscallFuncType = uint64_t (uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-uint64_t Syscall0(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6) {
-  Log(kWarn, "Syscall0(%lu, %lu, %lu, %lu, %lu, %lu)\n",
-      arg1, arg2, arg3, arg4, arg5, arg6);
-  return 0;
-}
-
-uint64_t Syscall2(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6) {
-  Log(kWarn, "Syscall2(%lu, %lu, %lu, %lu, %lu, %lu)\n",
-      arg1, arg2, arg3, arg4, arg5, arg6);
-  return 0;
-}
-
-extern "C" std::array<SyscallFuncType*, 3> syscall_table{
-  Syscall0,
-  nullptr,
-  Syscall2,
+extern "C" std::array<SyscallFuncType*, 2> syscall_table{
+  /* 0x00 */ syscall::PutChar,
+  /* 0x01 */ syscall::PutString,
 };
