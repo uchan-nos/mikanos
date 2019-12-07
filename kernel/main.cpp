e9fe693690b846c5b60562c0627dddeb689c379f
uchan-nos <uchan0@gmail.com>
2019-12-07 13:46:33 +0900

    pass syscall number using rax

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -155,7 +155,7 @@ extern "C" void KernelMainNewStack(
 
   WriteMSR(kIA32_EFER, 0x0501u);
   WriteMSR(kIA32_STAR, static_cast<uint64_t>(8) << 32 | static_cast<uint64_t>(16) << 48);
-  WriteMSR(kIA32_LSTAR, reinterpret_cast<uint64_t>(SyscallEntryAsm));
+  WriteMSR(kIA32_LSTAR, reinterpret_cast<uint64_t>(SyscallEntry));
   WriteMSR(kIA32_FMASK, 0);
 
   InitializeTask();
