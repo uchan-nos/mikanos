789f0b32996ad73a8ddd4d70517a7cd7b510b4b1
uchan-nos <uchan0@gmail.com>
2019-12-07 13:46:33 +0900

    pass syscall number using rax

--- apps/rpn/rpn.cpp
+++ apps/rpn/rpn.cpp
@@ -16,7 +16,8 @@ void Push(long value) {
   stack[stack_ptr] = value;
 }
 
-extern "C" uint64_t Syscall(uint64_t arg1, uint64_t arg2 = 0, uint64_t arg3 = 0, uint64_t arg4 = 0, uint64_t arg5 = 0);
+extern "C" uint64_t Syscall0(uint64_t arg1, uint64_t arg2 = 0, uint64_t arg3 = 0, uint64_t arg4 = 0, uint64_t arg5 = 0, uint64_t arg6 = 0);
+extern "C" uint64_t Syscall2(uint64_t arg1, uint64_t arg2 = 0, uint64_t arg3 = 0, uint64_t arg4 = 0, uint64_t arg5 = 0, uint64_t arg6 = 0);
 
 extern "C" int main(int argc, char** argv) {
   stack_ptr = -1;
@@ -26,22 +27,22 @@ extern "C" int main(int argc, char** argv) {
       long b = Pop();
       long a = Pop();
       Push(a + b);
-      Syscall(1, stack_ptr, a + b);
+      Syscall0(1, stack_ptr, a + b);
     } else if (strcmp(argv[i], "-") == 0) {
       long b = Pop();
       long a = Pop();
       Push(a - b);
-      Syscall(1, stack_ptr, a - b);
+      Syscall0(1, stack_ptr, a - b);
     } else {
       long a = atol(argv[i]);
       Push(a);
-      Syscall(1, stack_ptr, a);
+      Syscall0(1, stack_ptr, a);
     }
   }
   if (stack_ptr < 0) {
     return 0;
   }
-  Syscall(2);
+  Syscall2(2);
   while (1);
   //return static_cast<int>(Pop());
 }
--- kernel/asmfunc.h
+++ kernel/asmfunc.h
@@ -16,5 +16,5 @@ extern "C" {
   void CallApp(int argc, char** argv, uint16_t cs, uint16_t ss, uint64_t rip, uint64_t rsp);
   void SetSyscallRegs(uint64_t lstar, uint64_t fmask);
   void WriteMSR(uint32_t msr, uint64_t value);
-  void SyscallEntryAsm();
+  void SyscallEntry(void);
 }
