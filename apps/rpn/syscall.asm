6ce61b6ba8cd588fe05f4b5a7e6eca5ba15f1780
uchan-nos <uchan0@gmail.com>
2019-12-14 21:59:01 +0900

    don't use assembly macro

--- apps/rpn/syscall.asm
+++ apps/rpn/syscall.asm
@@ -1,13 +1,9 @@
 bits 64
 section .text
 
-%macro define_syscall 2
-global Syscall%1
-Syscall%1:
-    mov rax, %2
+global SyscallLogString
+SyscallLogString:
+    mov rax, 0x80000000
     mov r10, rcx
     syscall
     ret
-%endmacro
-
-define_syscall LogString, 0x80000000
