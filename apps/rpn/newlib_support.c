67daa608d612b64a0bc0213baefd11bb35ac4834
uchan-nos <uchan0@gmail.com>
2020-01-08 17:18:18 +0900

    re-order func definitions

--- apps/rpn/newlib_support.c
+++ apps/rpn/newlib_support.c
@@ -3,24 +3,17 @@
 #include <sys/stat.h>
 #include <stdint.h>
 
-#define SBRK_BUF_SIZE (2 * 1024)
-unsigned char sbrk_buf[SBRK_BUF_SIZE];
-off_t sbrk_index;
-
-int64_t SyscallPutString(uint64_t, uint64_t, uint64_t);
-
-caddr_t sbrk(int incr) {
-  if (sbrk_index + incr >= SBRK_BUF_SIZE) {
-    errno = ENOMEM;
-    return (caddr_t)-1;
-  }
-
-  off_t prev_index = sbrk_index;
-  sbrk_index += incr;
-  return (caddr_t)&sbrk_buf[prev_index];
+int close(int fd) {
+  errno = EBADF;
+  return -1;
 }
 
-int close(int fd) {
+int fstat(int fd, struct stat* buf) {
+  errno = EBADF;
+  return -1;
+}
+
+int isatty(int fd) {
   errno = EBADF;
   return -1;
 }
@@ -35,21 +28,28 @@ ssize_t read(int fd, void* buf, size_t count) {
   return -1;
 }
 
+#define SBRK_BUF_SIZE (2 * 1024)
+unsigned char sbrk_buf[SBRK_BUF_SIZE];
+off_t sbrk_index;
+
+caddr_t sbrk(int incr) {
+  if (sbrk_index + incr >= SBRK_BUF_SIZE) {
+    errno = ENOMEM;
+    return (caddr_t)-1;
+  }
+
+  off_t prev_index = sbrk_index;
+  sbrk_index += incr;
+  return (caddr_t)&sbrk_buf[prev_index];
+}
+
+int64_t SyscallPutString(uint64_t, uint64_t, uint64_t);
+
 ssize_t write(int fd, const void* buf, size_t count) {
   int64_t n = SyscallPutString(fd, (uint64_t)buf, count);
   if (n >= 0) {
     return n;
   }
   errno = -n;
-  return -1;
-}
-
-int fstat(int fd, struct stat* buf) {
-  errno = EBADF;
-  return -1;
-}
-
-int isatty(int fd) {
-  errno = EBADF;
   return -1;
 }
