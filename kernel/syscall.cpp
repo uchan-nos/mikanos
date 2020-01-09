5dfdf9ae88a689684b93391881769e3b78470691
uchan-nos <uchan0@gmail.com>
2020-01-09 11:50:36 +0900

    syscall returns a pair of value & error

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -11,22 +11,27 @@
 #include "terminal.hpp"
 
 namespace syscall {
+  struct Result {
+    uint64_t value;
+    int error;
+  };
 
 #define SYSCALL(name) \
-  int64_t name( \
+  Result name( \
       uint64_t arg1, uint64_t arg2, uint64_t arg3, \
       uint64_t arg4, uint64_t arg5, uint64_t arg6)
 
 SYSCALL(LogString) {
   if (arg1 != kError && arg1 != kWarn && arg1 != kInfo && arg1 != kDebug) {
-    return -1;
+    return { 0, EPERM };
   }
   const char* s = reinterpret_cast<const char*>(arg2);
-  if (strlen(s) > 1024) {
-    return -1;
+  const auto len = strlen(s);
+  if (len > 1024) {
+    return { 0, E2BIG };
   }
   Log(static_cast<LogLevel>(arg1), "%s", s);
-  return 0;
+  return { len, 0 };
 }
 
 SYSCALL(PutString) {
@@ -34,23 +39,23 @@ SYSCALL(PutString) {
   const char* s = reinterpret_cast<const char*>(arg2);
   const auto len = arg3;
   if (len > 1024) {
-    return -EIO;
+    return { 0, E2BIG };
   }
 
   if (fd == 1) {
     const auto task_id = task_manager->CurrentTask().ID();
     (*terminals)[task_id]->Print(s);
-    return len;
+    return { len, 0 };
   }
-  return -EBADF;
+  return { 0, EBADF };
 }
 
 #undef SYSCALL
 
 } // namespace syscall
 
-using SyscallFuncType = int64_t (uint64_t, uint64_t, uint64_t,
-                                 uint64_t, uint64_t, uint64_t);
+using SyscallFuncType = syscall::Result (uint64_t, uint64_t, uint64_t,
+                                         uint64_t, uint64_t, uint64_t);
 extern "C" std::array<SyscallFuncType*, 2> syscall_table{
   /* 0x00 */ syscall::LogString,
   /* 0x01 */ syscall::PutString,
