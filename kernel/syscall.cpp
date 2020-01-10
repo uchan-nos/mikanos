94e38e2bc9fd8c8e744319e712ca8a331cc1b720
uchan-nos <uchan0@gmail.com>
2020-01-10 09:33:43 +0900

    remove review tags

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -10,7 +10,6 @@
 #include "task.hpp"
 #include "terminal.hpp"
 
-// #@@range_begin(syscall_result)
 namespace syscall {
   struct Result {
     uint64_t value;
@@ -21,9 +20,7 @@ namespace syscall {
   Result name( \
       uint64_t arg1, uint64_t arg2, uint64_t arg3, \
       uint64_t arg4, uint64_t arg5, uint64_t arg6)
-// #@@range_end(syscall_result)
 
-// #@@range_begin(syscall_log_string)
 SYSCALL(LogString) {
   if (arg1 != kError && arg1 != kWarn && arg1 != kInfo && arg1 != kDebug) {
     return { 0, EPERM };
@@ -36,9 +33,7 @@ SYSCALL(LogString) {
   Log(static_cast<LogLevel>(arg1), "%s", s);
   return { len, 0 };
 }
-// #@@range_end(syscall_log_string)
 
-// #@@range_begin(syscall_put_string)
 SYSCALL(PutString) {
   const auto fd = arg1;
   const char* s = reinterpret_cast<const char*>(arg2);
@@ -54,20 +49,17 @@ SYSCALL(PutString) {
   }
   return { 0, EBADF };
 }
-// #@@range_end(syscall_put_string)
 
 #undef SYSCALL
 
 } // namespace syscall
 
-// #@@range_begin(syscall_table)
 using SyscallFuncType = syscall::Result (uint64_t, uint64_t, uint64_t,
                                          uint64_t, uint64_t, uint64_t);
 extern "C" std::array<SyscallFuncType*, 2> syscall_table{
   /* 0x00 */ syscall::LogString,
   /* 0x01 */ syscall::PutString,
 };
-// #@@range_end(syscall_table)
 
 void InitializeSyscall() {
   WriteMSR(kIA32_EFER, 0x0501u);
--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -223,9 +223,7 @@ Error CleanPageMaps(LinearAddress4Level addr) {
 
 } // namespace
 
-// #@@range_begin(term_task_id)
 Terminal::Terminal(uint64_t task_id) : task_id_{task_id} {
-// #@@range_end(term_task_id)
   window_ = std::make_shared<ToplevelWindow>(
       kColumns * 8 + 8 + ToplevelWindow::kMarginX,
       kRows * 16 + 8 + ToplevelWindow::kMarginY,
@@ -486,7 +484,6 @@ void Terminal::Print(char c) {
   }
 }
 
-// #@@range_begin(print_redraw)
 void Terminal::Print(const char* s, std::optional<size_t> len) {
   const auto cursor_before = CalcCursorPos();
   DrawCursor(false);
@@ -518,7 +515,6 @@ void Terminal::Print(const char* s, std::optional<size_t> len) {
   task_manager->SendMessage(1, msg);
   __asm__("sti");
 }
-// #@@range_end(print_redraw)
 
 Rectangle<int> Terminal::HistoryUpDown(int direction) {
   if (direction == -1 && cmd_history_index_ >= 0) {
@@ -546,7 +542,6 @@ Rectangle<int> Terminal::HistoryUpDown(int direction) {
   return draw_area;
 }
 
-// #@@range_begin(task_term_map)
 std::map<uint64_t, Terminal*>* terminals;
 
 void TaskTerminal(uint64_t task_id, int64_t data) {
@@ -558,7 +553,6 @@ void TaskTerminal(uint64_t task_id, int64_t data) {
   layer_task_map->insert(std::make_pair(terminal->LayerID(), task_id));
   (*terminals)[task_id] = terminal;
   __asm__("sti");
-// #@@range_end(task_term_map)
 
   while (true) {
     __asm__("cli");
