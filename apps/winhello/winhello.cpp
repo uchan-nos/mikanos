bb5e56dcfa59e5c3b14136e5a2762aab85db0b28
uchan-nos <uchan0@gmail.com>
2020-01-14 21:21:15 +0900

    use exit() insted of SyscallExit()

--- apps/winhello/winhello.cpp
+++ apps/winhello/winhello.cpp
@@ -1,14 +1,15 @@
+#include <cstdlib>
 #include "../syscall.h"
 
 extern "C" void main(int argc, char** argv) {
   auto [layer_id, err_openwin]
     = SyscallOpenWindow(200, 100, 10, 10, "winhello");
   if (err_openwin) {
-    SyscallExit(err_openwin);
+    exit(err_openwin);
   }
 
   SyscallWinWriteString(layer_id, 7, 24, 0xc00000, "hello world!");
   SyscallWinWriteString(layer_id, 24, 40, 0x00c000, "hello world!");
   SyscallWinWriteString(layer_id, 40, 56, 0x0000c0, "hello world!");
-  SyscallExit(0);
+  exit(0);
 }
