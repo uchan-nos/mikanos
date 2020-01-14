3e73bead4e303b44d23687d0a2fe2685aa2421be
uchan-nos <uchan0@gmail.com>
2020-01-14 21:14:12 +0900

    add syscall to write string on a window

--- apps/syscall.h
+++ apps/syscall.h
@@ -13,5 +13,6 @@ SyscallResult SyscallLogString(LogLevel level, const char* message);
 SyscallResult SyscallPutString(int fd, const char* s, size_t len);
 void SyscallExit(int exit_code);
 SyscallResult SyscallOpenWindow(int w, int h, int x, int y, const char* title);
+SyscallResult SyscallWinWriteString(unsigned int layer_id, int x, int y, uint32_t color, const char* s);
 
 } // extern "C"
