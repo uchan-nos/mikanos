5b6930a475ceaf32c89c17565649b2194090e778
uchan-nos <uchan0@gmail.com>
2020-01-14 13:52:27 +0900

    add syscall to open window

--- apps/syscall.h
+++ apps/syscall.h
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <cstdint>
 #include "../kernel/logger.hpp"
 
@@ -8,8 +9,11 @@ struct SyscallResult {
   int error;
 };
 
+// #@@range_begin(open_window_decl)
 SyscallResult SyscallLogString(LogLevel level, const char* message);
 SyscallResult SyscallPutString(int fd, const char* s, size_t len);
 void SyscallExit(int exit_code);
+SyscallResult SyscallOpenWindow(int w, int h, int x, int y, const char* title);
+// #@@range_end(open_window_decl)
 
 } // extern "C"
