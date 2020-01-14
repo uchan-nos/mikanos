f1542efe032570b3698668b253d925420f5f8a01
uchan-nos <uchan0@gmail.com>
2020-01-14 21:20:59 +0900

    use syscall.h inside newlib_support.c

--- apps/newlib_support.c
+++ apps/newlib_support.c
@@ -2,6 +2,8 @@
 #include <sys/types.h>
 #include <stdint.h>
 
+#include "syscall.h"
+
 int close(int fd) {
   errno = EBADF;
   return -1;
@@ -32,17 +34,15 @@ caddr_t sbrk(int incr) {
   return (caddr_t)-1;
 }
 
-struct SyscallResult {
-  uint64_t value;
-  int error;
-};
-struct SyscallResult SyscallPutString(uint64_t, uint64_t, uint64_t);
-
 ssize_t write(int fd, const void* buf, size_t count) {
-  struct SyscallResult res = SyscallPutString(fd, (uint64_t)buf, count);
+  struct SyscallResult res = SyscallPutString(fd, buf, count);
   if (res.error == 0) {
     return res.value;
   }
   errno = res.error;
   return -1;
+}
+
+void _exit(int status) {
+  SyscallExit(status);
 }
--- kernel/logger.hpp
+++ kernel/logger.hpp
@@ -18,7 +18,7 @@ enum LogLevel {
  * グローバルなログ優先度のしきい値を level に設定する．
  * 以降の Log の呼び出しでは，ここで設定した優先度以上のログのみ記録される．
  */
-void SetLogLevel(LogLevel level);
+void SetLogLevel(enum LogLevel level);
 
 /** @brief ログを指定された優先度で記録する．
  *
@@ -28,4 +28,4 @@ void SetLogLevel(LogLevel level);
  * @param level  ログの優先度．しきい値以上の優先度のログのみが記録される．
  * @param format  書式文字列．printk と互換．
  */
-int Log(LogLevel level, const char* format, ...);
+int Log(enum LogLevel level, const char* format, ...);
