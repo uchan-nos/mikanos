5299af47d9ad5864ec442b9bbf03ca19524bd538
uchan-nos <uchan0@gmail.com>
2020-01-18 08:01:11 +0900

    add syscall to fill rectangle on a window

--- apps/syscall.asm
+++ apps/syscall.asm
@@ -10,8 +10,9 @@ Syscall%1:
     ret
 %endmacro
 
-define_syscall LogString,      0x80000000
-define_syscall PutString,      0x80000001
-define_syscall Exit,           0x80000002
-define_syscall OpenWindow,     0x80000003
-define_syscall WinWriteString, 0x80000004
+define_syscall LogString,        0x80000000
+define_syscall PutString,        0x80000001
+define_syscall Exit,             0x80000002
+define_syscall OpenWindow,       0x80000003
+define_syscall WinWriteString,   0x80000004
+define_syscall WinFillRectangle, 0x80000005
