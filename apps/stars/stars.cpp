151c6c5b34dfe26af07585d4465bfd146b975f50
uchan-nos <uchan0@gmail.com>
2020-01-20 07:41:39 +0900

    paint many stars

--- apps/stars/stars.cpp
+++ apps/stars/stars.cpp
@@ -1,13 +1,26 @@
 #include <cstdlib>
 #include "../syscall.h"
 
+static constexpr int kWidth = 100, kHeight = 100;
+
 extern "C" void main(int argc, char** argv) {
   auto [layer_id, err_openwin]
-    = SyscallOpenWindow(100, 100, 10, 10, "stars");
+    = SyscallOpenWindow(kWidth + 8, kHeight + 28, 10, 10, "stars");
   if (err_openwin) {
     exit(err_openwin);
   }
 
-  SyscallWinFillRectangle(layer_id, 2, 10, 10, 20, 0xcc00cc);
+  SyscallWinFillRectangle(layer_id, 4, 24, kWidth, kHeight, 0x000000);
+
+  int num_stars = 100;
+  if (argc >= 2) {
+    num_stars = atoi(argv[1]);
+  }
+
+  for (int i = 0; i < num_stars; ++i) {
+    int x = rand() % (kWidth - 2);
+    int y = rand() % (kHeight - 2);
+    SyscallWinFillRectangle(layer_id, 4 + x, 24 + y, 2, 2, 0xfff100);
+  }
   exit(0);
 }
