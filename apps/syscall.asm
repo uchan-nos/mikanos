96a123fbfe93b42bd7d780d8cc4745e4aa9881d4
uchan-nos <uchan0@gmail.com>
2020-01-21 11:16:25 +0900

    measure time to paint all stars

--- apps/syscall.asm
+++ apps/syscall.asm
@@ -16,3 +16,4 @@ define_syscall Exit,             0x80000002
 define_syscall OpenWindow,       0x80000003
 define_syscall WinWriteString,   0x80000004
 define_syscall WinFillRectangle, 0x80000005
+define_syscall GetCurrentTick,   0x80000006
