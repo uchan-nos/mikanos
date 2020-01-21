ee3edaf5469ad35e4b1fdbaca66764899b5d3a8e
uchan-nos <uchan0@gmail.com>
2020-01-21 11:16:25 +0900

    measure time to paint all stars

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -10,6 +10,7 @@
 #include "task.hpp"
 #include "terminal.hpp"
 #include "font.hpp"
+#include "timer.hpp"
 
 namespace syscall {
   struct Result {
@@ -117,19 +118,26 @@ SYSCALL(WinFillRectangle) {
       }, arg1, arg2, arg3, arg4, arg5, arg6);
 }
 
+// #@@range_begin(get_current_tick)
+SYSCALL(GetCurrentTick) {
+  return { timer_manager->CurrentTick(), kTimerFreq };
+}
+// #@@range_end(get_current_tick)
+
 #undef SYSCALL
 
 } // namespace syscall
 
 using SyscallFuncType = syscall::Result (uint64_t, uint64_t, uint64_t,
                                          uint64_t, uint64_t, uint64_t);
-extern "C" std::array<SyscallFuncType*, 6> syscall_table{
+extern "C" std::array<SyscallFuncType*, 7> syscall_table{
   /* 0x00 */ syscall::LogString,
   /* 0x01 */ syscall::PutString,
   /* 0x02 */ syscall::Exit,
   /* 0x03 */ syscall::OpenWindow,
   /* 0x04 */ syscall::WinWriteString,
   /* 0x05 */ syscall::WinFillRectangle,
+  /* 0x06 */ syscall::GetCurrentTick,
 };
 
 void InitializeSyscall() {
