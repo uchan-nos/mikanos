8b859a77716145fe65b68300d12b24dfb04987e3
uchan-nos <uchan0@gmail.com>
2020-01-21 13:49:44 +0900

    add no-redraw option to window syscalls

--- apps/syscall.asm
+++ apps/syscall.asm
@@ -17,3 +17,4 @@ define_syscall OpenWindow,       0x80000003
 define_syscall WinWriteString,   0x80000004
 define_syscall WinFillRectangle, 0x80000005
 define_syscall GetCurrentTick,   0x80000006
+define_syscall WinRedraw,        0x80000007
