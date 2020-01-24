a7d6f4725b80221334cf409548bc9693db4be78e
uchan-nos <uchan0@gmail.com>
2020-01-24 11:02:45 +0900

    remove review tags

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -63,7 +63,6 @@ Layer& LayerManager::NewLayer() {
   return *layers_.emplace_back(new Layer{latest_id_});
 }
 
-// #@@range_begin(remove_layer)
 void LayerManager::RemoveLayer(unsigned int id) {
   Hide(id);
 
@@ -72,7 +71,6 @@ void LayerManager::RemoveLayer(unsigned int id) {
   };
   std::erase_if(layers_, pred);
 }
-// #@@range_end(remove_layer)
 
 void LayerManager::Draw(const Rectangle<int>& area) const {
   for (auto layer : layer_stack_) {
