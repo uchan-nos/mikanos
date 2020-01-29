0a9a83d628cc8b9de49cde42177b900de2b67e82
uchan-nos <uchan0@gmail.com>
2020-01-29 19:59:08 +0900

    generate events along with mouse movement

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -239,6 +239,17 @@ SYSCALL(ReadEvent) {
         ++i;
       }
       break;
+    // #@@range_begin(read_event_mouse)
+    case Message::kMouseMove:
+      app_events[i].type = AppEvent::kMouseMove;
+      app_events[i].arg.mouse_move.x = msg->arg.mouse_move.x;
+      app_events[i].arg.mouse_move.y = msg->arg.mouse_move.y;
+      app_events[i].arg.mouse_move.dx = msg->arg.mouse_move.dx;
+      app_events[i].arg.mouse_move.dy = msg->arg.mouse_move.dy;
+      app_events[i].arg.mouse_move.buttons = msg->arg.mouse_move.buttons;
+      ++i;
+      break;
+    // #@@range_end(read_event_mouse)
     default:
       Log(kInfo, "uncaught event type: %u\n", msg->type);
     }
