c331a10b5d487e5987b1f07de5c6915f18a7ec55
uchan-nos <uchan0@gmail.com>
2020-01-29 19:59:08 +0900

    generate events along with mouse movement

--- kernel/app_event.hpp
+++ kernel/app_event.hpp
@@ -4,11 +4,22 @@
 extern "C" {
 #endif
 
+// #@@range_begin(app_event)
 struct AppEvent {
   enum Type {
     kQuit,
+    kMouseMove,
   } type;
+
+  union {
+    struct {
+      int x, y;
+      int dx, dy;
+      uint8_t buttons;
+    } mouse_move;
+  } arg;
 };
+// #@@range_end(app_event)
 
 #ifdef __cplusplus
 } // extern "C"
