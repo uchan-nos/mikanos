d6576ab8d94dd607a9e06cae7ed43e113e40040a
uchan-nos <uchan0@gmail.com>
2020-02-01 17:12:00 +0900

    remove review tags

--- kernel/app_event.hpp
+++ kernel/app_event.hpp
@@ -5,13 +5,11 @@ extern "C" {
 #endif
 
 struct AppEvent {
-  // #@@range_begin(app_event_type)
   enum Type {
     kQuit,
     kMouseMove,
     kMouseButton,
   } type;
-  // #@@range_end(app_event_type)
 
   union {
     struct {
@@ -20,13 +18,11 @@ struct AppEvent {
       uint8_t buttons;
     } mouse_move;
 
-    // #@@range_begin(mouse_button_arg)
     struct {
       int x, y;
       int press; // 1: press, 0: release
       int button;
     } mouse_button;
-    // #@@range_end(mouse_button_arg)
   } arg;
 };
 
