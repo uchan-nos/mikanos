1caea8c03abf62399c0a1800b49bddba67581e71
uchan-nos <uchan0@gmail.com>
2020-02-01 15:57:01 +0900

    support mouse button event

--- kernel/message.hpp
+++ kernel/message.hpp
@@ -12,6 +12,7 @@ struct Message {
     kLayer,
     kLayerFinish,
     kMouseMove,
+    kMouseButton,
   } type;
 
   uint64_t src_task;
@@ -40,5 +41,11 @@ struct Message {
       int dx, dy;
       uint8_t buttons;
     } mouse_move;
+
+    struct {
+      int x, y;
+      int press; // 1: press, 0: release
+      int button;
+    } mouse_button;
   } arg;
 };
