a7f4ff114c5281603c9b955b6a6de80a68ee40fa
uchan-nos <uchan0@gmail.com>
2020-02-12 09:49:12 +0900

    ReadEvent() handles key press & release events

--- kernel/keyboard.cpp
+++ kernel/keyboard.cpp
@@ -40,9 +40,10 @@ const char keycode_map_shifted[256] = {
 
 } // namespace
 
+// #@@range_begin(register_key_observer)
 void InitializeKeyboard() {
   usb::HIDKeyboardDriver::default_observer =
-    [](uint8_t modifier, uint8_t keycode) {
+    [](uint8_t modifier, uint8_t keycode, bool press) {
       const bool shift = (modifier & (kLShiftBitMask | kRShiftBitMask)) != 0;
       char ascii = keycode_map[keycode];
       if (shift) {
@@ -52,6 +53,8 @@ void InitializeKeyboard() {
       msg.arg.keyboard.modifier = modifier;
       msg.arg.keyboard.keycode = keycode;
       msg.arg.keyboard.ascii = ascii;
+      msg.arg.keyboard.press = press;
       task_manager->SendMessage(1, msg);
     };
 }
+// #@@range_end(register_key_observer)
