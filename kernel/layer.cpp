428a95cbdd821330a6d8adf89014be32763e6bab
uchan-nos <uchan0@gmail.com>
2020-02-18 12:00:27 +0900

    stop cursor blinking when window deactivated

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include "console.hpp"
 #include "logger.hpp"
+#include "task.hpp"
 
 namespace {
   template <class T, class U>
@@ -205,6 +206,17 @@ int LayerManager::GetHeight(unsigned int id) {
 
 namespace {
   FrameBuffer* screen;
+
+  Error SendWindowActiveMessage(unsigned int layer_id, int activate) {
+    auto task_it = layer_task_map->find(layer_id);
+    if (task_it == layer_task_map->end()) {
+      return MAKE_ERROR(Error::kNoSuchTask);
+    }
+
+    Message msg{Message::kWindowActive};
+    msg.arg.window_active.activate = activate;
+    return task_manager->SendMessage(task_it->second, msg);
+  }
 }
 
 LayerManager* layer_manager;
@@ -225,6 +237,7 @@ void ActiveLayer::Activate(unsigned int layer_id) {
     Layer* layer = manager_.FindLayer(active_layer_);
     layer->GetWindow()->Deactivate();
     manager_.Draw(active_layer_);
+    SendWindowActiveMessage(active_layer_, 0);
   }
 
   active_layer_ = layer_id;
@@ -233,6 +246,7 @@ void ActiveLayer::Activate(unsigned int layer_id) {
     layer->GetWindow()->Activate();
     manager_.UpDown(active_layer_, manager_.GetHeight(mouse_layer_) - 1);
     manager_.Draw(active_layer_);
+    SendWindowActiveMessage(active_layer_, 1);
   }
 }
 
