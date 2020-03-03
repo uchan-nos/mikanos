ad69f73dcc1b18451f2fa5284d1b886bb7852216
uchan-nos <uchan0@gmail.com>
2020-03-03 19:51:44 +0900

    add grep command

---
+++ apps/grep/grep.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <cstdlib>
+#include <regex>
+
+extern "C" void main(int argc, char** argv) {
+  if (argc < 3) {
+    printf("Usage: %s <pattern> <file>\n", argv[0]);
+    exit(1);
+  }
+
+  std::regex pattern{argv[1]};
+
+  FILE* fp = fopen(argv[2], "r");
+  if (fp == nullptr) {
+    printf("failed to open: %s\n", argv[2]);
+    exit(1);
+  }
+
+  char line[256];
+  while (fgets(line, sizeof(line), fp)) {
+    std::cmatch m;
+    if (std::regex_search(line, m, pattern)) {
+      printf("%s", line);
+    }
+  }
+  exit(0);
+}
