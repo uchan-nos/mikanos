ce1d136f78e156f45f58ab5e9fc3949d81b3aa50
uchan-nos <uchan0@gmail.com>
2020-03-05 11:06:22 +0900

    Ctrl-D sends EOT

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -11,6 +11,7 @@
 #include "memory_manager.hpp"
 #include "paging.hpp"
 #include "timer.hpp"
+#include "keyboard.hpp"
 
 namespace {
 
@@ -728,11 +729,22 @@ size_t TerminalFileDescriptor::Read(void* buf, size_t len) {
     }
     __asm__("sti");
 
-    if (msg->type == Message::kKeyPush && msg->arg.keyboard.press) {
-      bufc[0] = msg->arg.keyboard.ascii;
-      term_.Print(bufc, 1);
-      return 1;
+    if (msg->type != Message::kKeyPush || !msg->arg.keyboard.press) {
+      continue;
     }
+    if (msg->arg.keyboard.modifier & (kLControlBitMask | kRControlBitMask)) {
+      char s[3] = "^ ";
+      s[1] = toupper(msg->arg.keyboard.ascii);
+      term_.Print(s);
+      if (msg->arg.keyboard.keycode == 7 /* D */) {
+        return 0; // EOT
+      }
+      continue;
+    }
+
+    bufc[0] = msg->arg.keyboard.ascii;
+    term_.Print(bufc, 1);
+    return 1;
   }
 }
 // #@@range_end(term_fd_read)
