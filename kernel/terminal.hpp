e9ee9e9e45a7a8baa3085c0104386c1abfa69859
uchan-nos <uchan0@gmail.com>
2020-03-07 12:26:00 +0900

    PutString syscall writes through FileDescriptor

--- kernel/terminal.hpp
+++ kernel/terminal.hpp
@@ -58,6 +58,7 @@ class TerminalFileDescriptor : public FileDescriptor {
  public:
   explicit TerminalFileDescriptor(Task& task, Terminal& term);
   size_t Read(void* buf, size_t len) override;
+  size_t Write(const void* buf, size_t len) override;
 
  private:
   Task& task_;
