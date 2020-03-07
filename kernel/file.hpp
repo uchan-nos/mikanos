e65c558fd13b35c0d410e456184968a9d22083b7
uchan-nos <uchan0@gmail.com>
2020-03-07 15:04:07 +0900

    add review tags

--- kernel/file.hpp
+++ kernel/file.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+// #@@range_begin(file_fd)
 class FileDescriptor {
  public:
   virtual ~FileDescriptor() = default;
   virtual size_t Read(void* buf, size_t len) = 0;
   virtual size_t Write(const void* buf, size_t len) = 0;
 };
+// #@@range_end(file_fd)
