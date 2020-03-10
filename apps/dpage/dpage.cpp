0e5edea0611ced5358c1f5aae01f07a7a7bcb575
uchan-nos <uchan0@gmail.com>
2020-03-10 16:44:04 +0900

    dpage command count specific bytes in a file

--- apps/dpage/dpage.cpp
+++ apps/dpage/dpage.cpp
@@ -3,19 +3,43 @@
 #include "../syscall.h"
 
 extern "C" void main(int argc, char** argv) {
-  char s[64];
-
-  SyscallResult res = SyscallDemandPages(1, 0);
-  int* p = reinterpret_cast<int*>(res.value);
-
-  sprintf(s, "finished to allocate pages: %p\n", p);
-  SyscallLogString(kWarn, s);
-
-  for (int i = 0; i < 4096 / sizeof(int); ++i) {
-    p[i] = i;
+  const char* filename = "/memmap";
+  int ch = '\n';
+  if (argc >= 3) {
+    filename = argv[1];
+    ch = atoi(argv[2]);
+  }
+  FILE* fp = fopen(filename, "r");
+  if (!fp) {
+    printf("failed to open %s\n", filename);
+    exit(1);
   }
 
-  SyscallLogString(kWarn, "finished to write\n");
+  SyscallResult res = SyscallDemandPages(1, 0);
+  if (res.error) {
+    exit(1);
+  }
+  char* buf = reinterpret_cast<char*>(res.value);
+  char* buf0 = buf;
 
+  size_t total = 0;
+  size_t n;
+  while ((n = fread(buf, 1, 4096, fp)) == 4096) {
+    total += n;
+    if (res = SyscallDemandPages(1, 0); res.error) {
+      exit(1);
+    }
+    buf += 4096;
+  }
+  total += n;
+  printf("size of %s = %lu bytes\n", filename, total);
+
+  size_t num = 0;
+  for (int i = 0; i < total; ++i) {
+    if (buf0[i] == ch) {
+      ++num;
+    }
+  }
+  printf("the number of '%c' (0x%02x) = %lu\n", ch, ch, num);
   exit(0);
 }
