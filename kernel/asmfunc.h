830833f106e946174e90c77369930ad623d4da3d
uchan-nos <uchan0@gmail.com>
2020-03-16 09:13:05 +0900

    copy on write for application executable

--- kernel/asmfunc.h
+++ kernel/asmfunc.h
@@ -10,6 +10,8 @@ extern "C" {
   void LoadGDT(uint16_t limit, uint64_t offset);
   void SetCSSS(uint16_t cs, uint16_t ss);
   void SetDSAll(uint16_t value);
+  uint64_t GetCR0();
+  void SetCR0(uint64_t value);
   uint64_t GetCR2();
   void SetCR3(uint64_t value);
   uint64_t GetCR3();
--- kernel/paging.hpp
+++ kernel/paging.hpp
@@ -91,6 +91,7 @@ union PageMapEntry {
 
 WithError<PageMapEntry*> NewPageMap();
 Error FreePageMap(PageMapEntry* table);
-Error SetupPageMaps(LinearAddress4Level addr, size_t num_4kpages);
+Error SetupPageMaps(LinearAddress4Level addr, size_t num_4kpages, bool writable = true);
 Error CleanPageMaps(LinearAddress4Level addr);
+Error CopyPageMaps(PageMapEntry* dest, PageMapEntry* src, int part, int start);
 Error HandlePageFault(uint64_t error_code, uint64_t causal_addr);
