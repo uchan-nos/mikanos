a3b3d8a123f3c377524823dd4c896469294cd030
uchan-nos <uchan0@gmail.com>
2020-03-20 23:31:21 +0900

    add WriteUnicode, ?? for zenkaku

--- kernel/font.hpp
+++ kernel/font.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
 #include <cstdint>
+#include <utility>
 #include "graphics.hpp"
 
 void WriteAscii(PixelWriter& writer, Vector2D<int> pos, char c, const PixelColor& color);
 void WriteString(PixelWriter& writer, Vector2D<int> pos, const char* s, const PixelColor& color);
+
+int CountUTF8Size(uint8_t c);
+std::pair<char32_t, int> ConvertUTF8To32(const char* u8);
+bool IsHankaku(char32_t c);
+void WriteUnicode(PixelWriter& writer, Vector2D<int> pos,
+                  char32_t c, const PixelColor& color);
