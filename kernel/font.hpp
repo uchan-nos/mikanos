a2220a4d0e22f894ab3ed0c146d7763229ef1b56
uchan-nos <uchan0@gmail.com>
2020-03-22 11:35:29 +0900

    put unicode char using FreeType

--- kernel/font.hpp
+++ kernel/font.hpp
@@ -2,7 +2,12 @@
 
 #include <cstdint>
 #include <utility>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 #include "graphics.hpp"
+#include "error.hpp"
 
 void WriteAscii(PixelWriter& writer, Vector2D<int> pos, char c, const PixelColor& color);
 void WriteString(PixelWriter& writer, Vector2D<int> pos, const char* s, const PixelColor& color);
@@ -10,5 +15,7 @@ void WriteString(PixelWriter& writer, Vector2D<int> pos, const char* s, const Pi
 int CountUTF8Size(uint8_t c);
 std::pair<char32_t, int> ConvertUTF8To32(const char* u8);
 bool IsHankaku(char32_t c);
-void WriteUnicode(PixelWriter& writer, Vector2D<int> pos,
-                  char32_t c, const PixelColor& color);
+WithError<FT_Face> NewFTFace();
+Error WriteUnicode(PixelWriter& writer, Vector2D<int> pos,
+                   char32_t c, const PixelColor& color);
+void InitializeFont();
--- kernel/task.hpp
+++ kernel/task.hpp
@@ -38,7 +38,7 @@ struct FileMapping {
 class Task {
  public:
   static const int kDefaultLevel = 1;
-  static const size_t kDefaultStackBytes = 4096;
+  static const size_t kDefaultStackBytes = 8 * 4096;
 
   Task(uint64_t id);
   Task& InitContext(TaskFunc* f, int64_t data);
