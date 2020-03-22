3c877ea0ce6770a39e153c9603d6aae5cb4e31f9
uchan-nos <uchan0@gmail.com>
2020-03-22 11:35:29 +0900

    put unicode char using FreeType

--- kernel/error.hpp
+++ kernel/error.hpp
@@ -34,6 +34,7 @@ class Error {
     kInvalidFile,
     kIsDirectory,
     kNoSuchEntry,
+    kFreeTypeError,
     kLastOfCode,  // この列挙子は常に最後に配置する
   };
 
@@ -67,6 +68,7 @@ class Error {
     "kInvalidFile",
     "kIsDirectory",
     "kNoSuchEntry",
+    "kFreeTypeError",
   };
   static_assert(Error::Code::kLastOfCode == code_names_.size());
 
