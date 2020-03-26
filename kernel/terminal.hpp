65c151b64802c729ece83c90a6065d2d9ef80ad9
uchan-nos <uchan0@gmail.com>
2020-03-26 12:07:54 +0900

    implement redirect

--- kernel/terminal.hpp
+++ kernel/terminal.hpp
@@ -6,8 +6,10 @@
 
 #pragma once
 
+#include <array>
 #include <deque>
 #include <map>
+#include <memory>
 #include <optional>
 #include "window.hpp"
 #include "task.hpp"
@@ -26,17 +28,20 @@ class Terminal {
   static const int kRows = 15, kColumns = 60;
   static const int kLineMax = 128;
 
-  Terminal(uint64_t task_id, bool show_window);
+  Terminal(Task& task, bool show_window);
   unsigned int LayerID() const { return layer_id_; }
   Rectangle<int> BlinkCursor();
   Rectangle<int> InputKey(uint8_t modifier, uint8_t keycode, char ascii);
 
   void Print(const char* s, std::optional<size_t> len = std::nullopt);
 
+  Task& UnderlyingTask() const { return task_; }
+  int LastExitCode() const { return last_exit_code_; }
+
  private:
   std::shared_ptr<ToplevelWindow> window_;
   unsigned int layer_id_;
-  uint64_t task_id_;
+  Task& task_;
 
   Vector2D<int> cursor_{0, 0};
   bool cursor_visible_{false};
@@ -48,27 +53,31 @@ class Terminal {
   void Scroll1();
 
   void ExecuteLine();
-  Error ExecuteFile(fat::DirectoryEntry& file_entry, char* command, char* first_arg);
+  WithError<int> ExecuteFile(fat::DirectoryEntry& file_entry,
+                             char* command, char* first_arg);
   void Print(char32_t c);
 
   std::deque<std::array<char, kLineMax>> cmd_history_{};
   int cmd_history_index_{-1};
   Rectangle<int> HistoryUpDown(int direction);
 
+// #@@range_begin(terminal_fields)
   bool show_window_;
+  std::array<std::shared_ptr<FileDescriptor>, 3> files_;
+  int last_exit_code_{0};
+// #@@range_end(terminal_fields)
 };
 
 void TaskTerminal(uint64_t task_id, int64_t data);
 
 class TerminalFileDescriptor : public FileDescriptor {
  public:
-  explicit TerminalFileDescriptor(Task& task, Terminal& term);
+  explicit TerminalFileDescriptor(Terminal& term);
   size_t Read(void* buf, size_t len) override;
   size_t Write(const void* buf, size_t len) override;
   size_t Size() const override { return 0; }
   size_t Load(void* buf, size_t len, size_t offset) override;
 
  private:
-  Task& task_;
   Terminal& term_;
 };
