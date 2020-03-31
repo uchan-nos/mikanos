b873a52ebbd8fb727e482c811d5132da6e005269
uchan-nos <uchan0@gmail.com>
2020-03-31 16:33:23 +0900

    TerminalFileDescriptor::Read/Write redraw terminal

--- kernel/file.cpp
+++ kernel/file.cpp
@@ -15,3 +15,19 @@ size_t PrintToFD(FileDescriptor& fd, const char* format, ...) {
   return result;
 }
 
+// #@@range_begin(readdelim)
+size_t ReadDelim(FileDescriptor& fd, char delim, char* buf, size_t len) {
+  size_t i = 0;
+  for (; i < len - 1; ++i) {
+    if (fd.Read(&buf[i], 1) == 0) {
+      break;
+    }
+    if (buf[i] == delim) {
+      ++i;
+      break;
+    }
+  }
+  buf[i] = '\0';
+  return i;
+}
+// #@@range_end(readdelim)
