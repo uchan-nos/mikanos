e461a5a3c18277ee29249b668c63d2f8e47c22eb
uchan-nos <uchan0@gmail.com>
2020-04-13 16:08:50 +0900

    save ctx to TaskContext instead of stack

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -76,6 +76,13 @@ SetCR3:
     mov cr3, rdi
     ret
 
+; #@@range_begin(get_cr3)
+global GetCR3  ; uint64_t GetCR3();
+GetCR3:
+    mov rax, cr3
+    ret
+; #@@range_end(get_cr3)
+
 extern kernel_main_stack
 extern KernelMainNewStack
 
@@ -89,41 +96,80 @@ KernelMain:
 
 ; #@@range_begin(switch_context)
 global SwitchContext
-SwitchContext:  ; void SwitchContext(uint64_t* to_rsp, uint64_t* current_rsp);
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rdi
-    push rsi
-    push rbp
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
+SwitchContext:  ; void SwitchContext(void* next_ctx, void* current_ctx);
+    mov [rsi + 0x40], rax
+    mov [rsi + 0x48], rbx
+    mov [rsi + 0x50], rcx
+    mov [rsi + 0x58], rdx
+    mov [rsi + 0x60], rdi
+    mov [rsi + 0x68], rsi
 
-    mov [rsi], rsp
-    mov rsp, [rdi]
+    lea rax, [rsp + 8]
+    mov [rsi + 0x70], rax  ; RSP
+    mov [rsi + 0x78], rbp
 
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rbp
-    pop rsi
-    pop rdi
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
+    mov [rsi + 0x80], r8
+    mov [rsi + 0x88], r9
+    mov [rsi + 0x90], r10
+    mov [rsi + 0x98], r11
+    mov [rsi + 0xa0], r12
+    mov [rsi + 0xa8], r13
+    mov [rsi + 0xb0], r14
+    mov [rsi + 0xb8], r15
 
-    ret
+    mov rax, cr3
+    mov [rsi + 0x00], rax  ; CR3
+    mov rax, [rsp]
+    mov [rsi + 0x08], rax  ; RIP
+    pushfq
+    pop qword [rsi + 0x10] ; RFLAGS
+
+    mov ax, cs
+    mov [rsi + 0x20], rax
+    mov bx, ss
+    mov [rsi + 0x28], rbx
+    mov cx, fs
+    mov [rsi + 0x30], rcx
+    mov dx, gs
+    mov [rsi + 0x38], rdx
+
+    fxsave [rsi + 0xc0]
+
+    and rsp, 0xfffffffffffffff0
+
+    ; iretq 用のスタックフレーム
+    push qword [rdi + 0x28] ; SS
+    push qword [rdi + 0x70] ; RSP
+    push qword [rdi + 0x10] ; RFLAGS
+    push qword [rdi + 0x20] ; CS
+    push qword [rdi + 0x08] ; RIP
+
+    ; コンテキストの復帰
+    fxrstor [rdi + 0xc0]
+
+    mov rax, [rdi + 0x00]
+    mov cr3, rax
+    mov rax, [rdi + 0x30]
+    mov fs, ax
+    mov rax, [rdi + 0x38]
+    mov gs, ax
+
+    mov rax, [rdi + 0x40]
+    mov rbx, [rdi + 0x48]
+    mov rcx, [rdi + 0x50]
+    mov rdx, [rdi + 0x58]
+    mov rsi, [rdi + 0x68]
+    mov rbp, [rdi + 0x78]
+    mov r8,  [rdi + 0x80]
+    mov r9,  [rdi + 0x88]
+    mov r10, [rdi + 0x90]
+    mov r11, [rdi + 0x98]
+    mov r12, [rdi + 0xa0]
+    mov r13, [rdi + 0xa8]
+    mov r14, [rdi + 0xb0]
+    mov r15, [rdi + 0xb8]
+
+    mov rdi, [rdi + 0x60]
+
+    iretq
 ; #@@range_end(switch_context)
