b47cf7f6fb354969c43011f18507397ad9e9f162
uchan-nos <uchan0@gmail.com>
2020-04-15 20:55:01 +0900

    remove review tags

--- kernel/task.cpp
+++ kernel/task.cpp
@@ -4,12 +4,9 @@
 #include "segment.hpp"
 #include "timer.hpp"
 
-// #@@range_begin(task_ctor)
 Task::Task(uint64_t id) : id_{id} {
 }
-// #@@range_end(task_ctor)
 
-// #@@range_begin(task_initctx)
 Task& Task::InitContext(TaskFunc* f, int64_t data) {
   const size_t stack_size = kDefaultStackBytes / sizeof(stack_[0]);
   stack_.resize(stack_size);
@@ -31,28 +28,20 @@ Task& Task::InitContext(TaskFunc* f, int64_t data) {
 
   return *this;
 }
-// #@@range_end(task_initctx)
 
-// #@@range_begin(task_context)
 TaskContext& Task::Context() {
   return context_;
 }
-// #@@range_end(task_context)
 
-// #@@range_begin(taskmgr_ctor)
 TaskManager::TaskManager() {
   NewTask();
 }
-// #@@range_end(taskmgr_ctor)
 
-// #@@range_begin(taskmgr_newtask)
 Task& TaskManager::NewTask() {
   ++latest_id_;
   return *tasks_.emplace_back(new Task{latest_id_});
 }
-// #@@range_end(taskmgr_newtask)
 
-// #@@range_begin(taskmgr_switchtask)
 void TaskManager::SwitchTask() {
   size_t next_task_index = current_task_index_ + 1;
   if (next_task_index >= tasks_.size()) {
@@ -65,11 +54,9 @@ void TaskManager::SwitchTask() {
 
   SwitchContext(&next_task.Context(), &current_task.Context());
 }
-// #@@range_end(taskmgr_switchtask)
 
 TaskManager* task_manager;
 
-// #@@range_begin(inittask)
 void InitializeTask() {
   task_manager = new TaskManager;
 
@@ -78,4 +65,3 @@ void InitializeTask() {
       Timer{timer_manager->CurrentTick() + kTaskTimerPeriod, kTaskTimerValue});
   __asm__("sti");
 }
-// #@@range_end(inittask)
