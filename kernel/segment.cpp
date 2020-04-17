16a6d2fea72dad1583f47a29641d3efc41b65e00
uchan-nos <uchan0@gmail.com>
2020-04-17 11:42:20 +0900

    set TSS for switching user to kernel mode

--- kernel/segment.cpp
+++ kernel/segment.cpp
@@ -1,9 +1,15 @@
 #include "segment.hpp"
 
 #include "asmfunc.h"
+#include "interrupt.hpp"
+#include "logger.hpp"
+#include "memory_manager.hpp"
 
 namespace {
-  std::array<SegmentDescriptor, 5> gdt;
+  std::array<SegmentDescriptor, 7> gdt;
+  std::array<uint32_t, 26> tss;
+
+  static_assert((kTSS >> 3) + 1 < gdt.size());
 }
 
 void SetCodeSegment(SegmentDescriptor& desc,
@@ -40,6 +46,16 @@ void SetDataSegment(SegmentDescriptor& desc,
   desc.bits.default_operation_size = 1; // 32-bit stack segment
 }
 
+void SetSystemSegment(SegmentDescriptor& desc,
+                      DescriptorType type,
+                      unsigned int descriptor_privilege_level,
+                      uint32_t base,
+                      uint32_t limit) {
+  SetCodeSegment(desc, type, descriptor_privilege_level, base, limit);
+  desc.bits.system_segment = 0;
+  desc.bits.long_mode = 0;
+}
+
 void SetupSegments() {
   gdt[0].data = 0;
   SetCodeSegment(gdt[1], DescriptorType::kExecuteRead, 0, 0, 0xfffff);
@@ -54,4 +70,24 @@ void InitializeSegmentation() {
 
   SetDSAll(kKernelDS);
   SetCSSS(kKernelCS, kKernelSS);
+}
+
+void InitializeTSS() {
+  const int kRSP0Frames = 8;
+  auto [ stack0, err ] = memory_manager->Allocate(kRSP0Frames);
+  if (err) {
+    Log(kError, "failed to allocate rsp0: %s\n", err.Name());
+    exit(1);
+  }
+  uint64_t rsp0 =
+    reinterpret_cast<uint64_t>(stack0.Frame()) + kRSP0Frames * 4096;
+  tss[1] = rsp0 & 0xffffffff;
+  tss[2] = rsp0 >> 32;
+
+  uint64_t tss_addr = reinterpret_cast<uint64_t>(&tss[0]);
+  SetSystemSegment(gdt[kTSS >> 3], DescriptorType::kTSSAvailable, 0,
+                   tss_addr & 0xffffffff, sizeof(tss)-1);
+  gdt[(kTSS >> 3) + 1].data = tss_addr >> 32;
+
+  LoadTR(kTSS);
 }
--- kernel/segment.hpp
+++ kernel/segment.hpp
@@ -44,6 +44,8 @@ void SetDataSegment(SegmentDescriptor& desc,
 const uint16_t kKernelCS = 1 << 3;
 const uint16_t kKernelSS = 2 << 3;
 const uint16_t kKernelDS = 0;
+const uint16_t kTSS = 5 << 3;
 
 void SetupSegments();
 void InitializeSegmentation();
+void InitializeTSS();
