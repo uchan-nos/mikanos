e6aafec39dec83e2ce50276ddd96c5dfaa9cf1cf
uchan-nos <uchan0@gmail.com>
2020-04-17 13:28:37 +0900

    save original context when interrupt

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -131,7 +131,10 @@ SwitchContext:  ; void SwitchContext(void* next_ctx, void* current_ctx);
     mov [rsi + 0x38], rdx
 
     fxsave [rsi + 0xc0]
+    ; fall through to RestoreContext
 
+global RestoreContext
+RestoreContext:  ; void RestoreContext(void* task_context);
     ; iret 用のスタックフレーム
     push qword [rdi + 0x28] ; SS
     push qword [rdi + 0x70] ; RSP
@@ -178,6 +181,72 @@ CallApp:  ; void CallApp(int argc, char** argv, uint16_t cs, uint16_t ss, uint64
     push r8   ; RIP
     o64 retf
     ; アプリケーションが終了してもここには来ない
+
+extern LAPICTimerOnInterrupt
+; void LAPICTimerOnInterrupt(const TaskContext& ctx_stack);
+
+global IntHandlerLAPICTimer
+IntHandlerLAPICTimer:  ; void IntHandlerLAPICTimer();
+    push rbp
+    mov rbp, rsp
+
+    ; スタック上に TaskContext 型の構造を構築する
+    sub rsp, 512
+    fxsave [rsp]
+    push r15
+    push r14
+    push r13
+    push r12
+    push r11
+    push r10
+    push r9
+    push r8
+    push qword [rbp]         ; RBP
+    push qword [rbp + 0x20]  ; RSP
+    push rsi
+    push rdi
+    push rdx
+    push rcx
+    push rbx
+    push rax
+
+    mov ax, fs
+    mov bx, gs
+    mov rcx, cr3
+
+    push rbx                 ; GS
+    push rax                 ; FS
+    push qword [rbp + 0x28]  ; SS
+    push qword [rbp + 0x10]  ; CS
+    push rbp                 ; reserved1
+    push qword [rbp + 0x18]  ; RFLAGS
+    push qword [rbp + 0x08]  ; RIP
+    push rcx                 ; CR3
+
+    mov rdi, rsp
+    call LAPICTimerOnInterrupt
+
+    add rsp, 8*8  ; CR3 から GS までを無視
+    pop rax
+    pop rbx
+    pop rcx
+    pop rdx
+    pop rdi
+    pop rsi
+    add rsp, 16   ; RSP, RBP を無視
+    pop r8
+    pop r9
+    pop r10
+    pop r11
+    pop r12
+    pop r13
+    pop r14
+    pop r15
+    fxrstor [rsp]
+
+    mov rsp, rbp
+    pop rbp
+    iretq
 
 global LoadTR
 LoadTR:  ; void LoadTR(uint16_t sel);
