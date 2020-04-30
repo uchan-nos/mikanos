6e357a107448e005bdd9b9c48130e483815b9417
uchan-nos <uchan0@gmail.com>
2020-04-30 11:46:43 +0900

    add null check for first_arg in ls command

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -414,7 +414,7 @@ void Terminal::ExecuteLine() {
     }
   // #@@range_begin(command_ls)
   } else if (strcmp(command, "ls") == 0) {
-    if (first_arg[0] == '\0') {
+    if (!first_arg || first_arg[0] == '\0') {
       ListAllEntries(this, fat::boot_volume_image->root_cluster);
     } else {
       auto [ dir, post_slash ] = fat::FindFile(first_arg);
