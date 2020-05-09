c9a1a88c9dff5d36d131522b741ca9824d5fcad4
uchan-nos <uchan0@gmail.com>
2020-05-09 10:08:42 +0900

    use make_mikanos_image.sh to build image file

--- build.sh
+++ build.sh
@@ -9,7 +9,9 @@ do
   make ${MAKE_OPTS:-} -C $APP_DIR $APP
 done
 
+DISK_IMG=./disk.img MIKANOS_DIR=$PWD $HOME/osbook/devenv/make_mikanos_image.sh
+
 if [ "${1:-}" = "run" ]
 then
-  MIKANOS_DIR=$PWD $HOME/osbook/devenv/run_mikanos.sh
+  $HOME/osbook/devenv/run_image.sh ./disk.img
 fi
