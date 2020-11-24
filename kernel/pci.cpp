d0f75cd02ae9b72ff9d79ed2e8deb0a0dadccdaa
uchan-nos <uchan0@gmail.com>
2020-11-24 22:40:59 +0900

    move MakeAddress to pci.cpp

--- kernel/pci.cpp
+++ kernel/pci.cpp
@@ -11,6 +11,20 @@
 namespace {
   using namespace pci;
 
+  /** @brief CONFIG_ADDRESS 用の 32 ビット整数を生成する */
+  constexpr uint32_t MakeAddress(uint8_t bus, uint8_t device,
+                                 uint8_t function, uint8_t reg_addr) {
+    auto shl = [](uint32_t x, unsigned int bits) {
+        return x << bits;
+    };
+
+    return shl(1, 31)  // enable bit
+        | shl(bus, 16)
+        | shl(device, 11)
+        | shl(function, 8)
+        | (reg_addr & 0xfcu);
+  }
+
   /** @brief devices[num_device] に情報を書き込み num_device をインクリメントする． */
   Error AddDevice(uint8_t bus, uint8_t device,
                   uint8_t function, uint8_t header_type) {
--- kernel/pci.hpp
+++ kernel/pci.hpp
@@ -17,20 +17,6 @@ namespace pci {
   /** @brief CONFIG_DATA レジスタの IO ポートアドレス */
   const uint16_t kConfigData = 0x0cfc;
 
-  /** @brief CONFIG_ADDRESS 用の 32 ビット整数を生成する */
-  constexpr uint32_t MakeAddress(uint8_t bus, uint8_t device,
-                                 uint8_t function, uint8_t reg_addr) {
-    auto shl = [](uint32_t x, unsigned int bits) {
-        return x << bits;
-    };
-
-    return shl(1, 31)  // enable bit
-        | shl(bus, 16)
-        | shl(device, 11)
-        | shl(function, 8)
-        | (reg_addr & 0xfcu);
-  }
-
   /** @brief CONFIG_ADDRESS に指定された整数を書き込む */
   void WriteAddress(uint32_t address);
   /** @brief CONFIG_DATA に指定された整数を書き込む */
