6e0e951a7efefde9dbdf389422dd952b2929c008
Keiji Suzuki <zuki.ebetsu@gmail.com>
2021-04-16 15:07:29 +0900

    TaskBウィンドウが消えるバグを修正

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -196,7 +196,6 @@ extern "C" void KernelMainNewStack(
   InitializeTextWindow();
   InitializeTaskBWindow();
   layer_manager->Draw({{0, 0}, ScreenSize()});
-  active_layer->Activate(task_b_window_layer_id);
 
   acpi::Initialize(acpi_table);
   InitializeLAPICTimer();
@@ -216,6 +215,7 @@ extern "C" void KernelMainNewStack(
   usb::xhci::Initialize();
   InitializeKeyboard();
   InitializeMouse();
+  active_layer->Activate(task_b_window_layer_id);
 
   char str[128];
 
