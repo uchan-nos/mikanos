fdb3ae8a3b9b005d6abebca9a2856be008dfabf4
MikeCAT <mikecat@users.noreply.github.com>
2022-09-22 15:10:08 +0000

    clear port_reset_change register in Port::Reset()

--- kernel/usb/xhci/port.cpp
+++ kernel/usb/xhci/port.cpp
@@ -31,7 +31,7 @@ namespace usb::xhci {
   Error Port::Reset() {
     auto portsc = port_reg_set_.PORTSC.Read();
     portsc.data[0] &= 0x0e00c3e0u;
-    portsc.data[0] |= 0x00020010u; // Write 1 to PR and CSC
+    portsc.data[0] |= 0x00220010u; // Write 1 to PR and CSC and PRC
     port_reg_set_.PORTSC.Write(portsc);
     while (port_reg_set_.PORTSC.Read().bits.port_reset);
     return MAKE_ERROR(Error::kSuccess);
